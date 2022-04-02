34fc5bae54377caaf910f547e3755fcab8d001ad
nobushi95 <nobushi1121@gmail.com>
2022-04-02 17:07:33 +0900

    add template/[A-D].cpp

---
+++ template/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    
+    return 0;
+}
