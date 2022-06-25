5fc2d51b4fa2ead838b435faafef283e8dc3dec4
nobushi95 <nobushi1121@gmail.com>
2022-06-25 21:24:19 +0900

    add abc227/B

---
+++ contest/abc227/B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> S(N);
+    FORE(s, S) cin >> s;
+
+    vector<bool> tbl(5000000, false);
+    REPEQ(a, 1, 1000) REPEQ(b, 1, 1000) tbl[4 * a * b + 3 * a + 3 * b] = true;
+
+    ll ans = 0;
+    FORE(s, S) if (!tbl[s]) ans++;
+
+    cout << ans << endl;
+
+    return 0;
+}
