35455c7c5a4426d9828ca515e88d673104ffa8b8
nobushi95 <nobushi1121@gmail.com>
2022-07-12 23:21:04 +0900

    add abc226/B

---
+++ contest/abc226/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> L(N);
+    vector<vector<ll>> a(N);
+    REP(i, 0, N) {
+        cin >> L[i];
+        a[i].resize(L[i]);
+        FORE(aij, a[i]) cin >> aij;
+    }
+
+    map<vector<ll>, ll> tbl;
+    REP(i, 0, N) tbl[a[i]]++;
+    cout << tbl.size() << endl;
+    
+    return 0;
+}
