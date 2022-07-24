35e9d43d797a40e9a1c74251baca8a3d0d37e2b1
nobushi95 <nobushi1121@gmail.com>
2022-07-24 11:23:16 +0900

    add abc261/E

---
+++ contest/abc261/E.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+#define bit(x, i) (((x) >> (i)) & 1)
+
+int main() {
+    ll N, C;
+    cin >> N >> C;
+    vector<ll> T(N), A(N);
+    REP(i, 0, N) cin >> T[i] >> A[i];
+
+    vector<ll> ans(N);
+    REP(k, 0, 30) {
+        array<ll, 2> fun = {0, 1};
+        ll cur = bit(C, k);
+        REP(i, 0, N) {
+            array<ll, 2> f;
+            ll x = bit(A[i], k);
+            ll op = T[i];
+            if      (op == 1) f = { 0 & x, 1 & x };
+            else if (op == 2) f = { 0 | x, 1 | x};
+            else if (op == 3) f = { 0 ^ x, 1 ^ x};
+            fun = { f[fun[0]], f[fun[1]] };
+            cur = fun[cur];
+            ans[i] |= cur << k;
+        }
+    }
+
+    REP(i, 0, N) cout << ans[i] << endl;
+
+    return 0;
+}
