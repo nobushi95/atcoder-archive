394f73b86486b9739909ae81e175cc33cc7b0b0f
nobushi95 <nobushi1121@gmail.com>
2022-08-03 23:31:33 +0900

    add abc258/A

---
+++ contest/abc258/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll K;
+    cin >> K;
+
+    ll h = 21 + K / 60;
+    ll m = K % 60;
+
+    cout <<  h << ":" << setfill('0') << right << setw(2) << m << endl;
+
+    return 0;
+}
