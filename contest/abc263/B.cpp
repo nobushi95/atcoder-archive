9e45c70f4aff96f0e89c13cbe4698dcb5b2fa3dd
nobushi95 <nobushi1121@gmail.com>
2022-08-07 14:23:08 +0900

    add abc263

---
+++ contest/abc263/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> P(N + 1);
+    REPEQ(i, 2, N) cin >> P[i];
+    
+    ll ans = 0;
+    ll cur = N;
+    while (cur != 1) {
+        cur = P[cur];
+        ans++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
