7f8f470414ece24f8730eaf642b1c1ea6379b2d6
nobushi95 <nobushi1121@gmail.com>
2022-08-28 12:27:08 +0900

    add abc266/E

---
+++ contest/abc266/E.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N;
+    cin >> N;
+
+    double ans = 3.5;
+    REP(i, 1, N) {
+        double cur = 0;
+        REPEQ(d, 1, 6) cur += max(ans, double(d)) / 6;
+        ans = cur;
+    }
+
+    cout << fixed << setprecision(7) << ans << endl;
+
+    return 0;
+}
