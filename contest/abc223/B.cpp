d876c29b6e285264e496a59067ec131b0011da6a
nobushi95 <nobushi1121@gmail.com>
2022-08-31 23:18:33 +0900

    add abc223/B

---
+++ contest/abc223/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    string S;
+    cin >> S;
+
+    ll N = S.length();
+    set<string> tbl;
+    REP(i, 0, N) {
+        string s;
+        REP(j, 0, N) {
+            ll idx = (i + j) % N;
+            s.push_back(S[idx]);
+        }
+        tbl.insert(s);
+    }
+
+    cout << *tbl.begin() << endl;
+    cout << *tbl.rbegin() << endl;
+
+    return 0;
+}
