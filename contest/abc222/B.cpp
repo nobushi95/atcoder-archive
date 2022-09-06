64f823be72e8a494acbfca0879a646689da4fc1a
nobushi95 <nobushi1121@gmail.com>
2022-09-06 23:26:17 +0900

    add abc222/B

---
+++ contest/abc222/B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll N, P;
+    cin >> N >> P;
+    vector<ll> a(N);
+    FORE(ai, a) cin >> ai;
+
+    auto f = [&P](auto &v){ return v < P; };
+    ll ans = count_if(ALL(a), f);
+    cout << ans << endl;
+    return 0;
+}
