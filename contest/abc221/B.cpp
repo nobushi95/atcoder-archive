4e3c1e67024fc3577b4627229e54394fb4c001dd
nobushi95 <nobushi1121@gmail.com>
2022-09-09 00:29:23 +0900

    add abc221/B

---
+++ contest/abc221/B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    string S, T;
+    cin >> S >> T;
+
+    if (S == T) {
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    REP(i, 0, S.length() - 1) {
+        auto s = S;
+        swap(s[i], s[i + 1]);
+        if (s == T) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+
+    cout << "No" << endl;
+
+    return 0;
+}
