5b3e25a437e502507f2766e89b540a30e735a498
nobushi95 <nobushi1121@gmail.com>
2022-09-26 23:45:32 +0900

    add abc217/B

---
+++ contest/abc217/B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    vector<string> S(3);
+    FORE(s, S) cin >> s;
+
+    const string contest[4] = {"ABC", "ARC", "AGC", "AHC"};
+    FORE(c, contest) if (find(ALL(S), c) == S.end()) cout << c << endl;
+
+    return 0;
+}
