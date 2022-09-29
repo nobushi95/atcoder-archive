0dbbb9fe61f73ad574d449cb0c207e9735e46fd6
nobushi95 <nobushi1121@gmail.com>
2022-09-29 23:33:34 +0900

    add abc216/A

---
+++ contest/abc216/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    string s;
+    cin >> s;
+
+    ll Y = s.back() - '0';
+    s.pop_back();
+    s.pop_back();
+    cout << s;
+    if (0 <= Y && Y <= 2) {
+        cout << "-" << endl;
+    } else if (3 <= Y && Y <= 6) {
+        cout << endl;
+    } else {
+        cout << "+" << endl;
+    }
+
+    return 0;
+}
