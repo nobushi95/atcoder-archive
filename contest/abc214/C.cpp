d8d99325689761126d005363d2df9a35ecb59f23
nobushi95 <nobushi1121@gmail.com>
2022-10-09 12:59:02 +0900

    add abc214/C

---
+++ contest/abc214/C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> S(N), T(N);
+    FORE(s, S) cin >> s;
+    FORE(t, T) cin >> t;
+
+    ll t = infl;
+    ll idx;
+    REP(i, 0, N) {
+        if (chmin(t, T[i])) idx = i;
+    }
+
+    vector<ll> ans(N, infl);
+    REP(i, 0, N) {
+        ll cur = (idx + i) % N;
+        ans[cur] = t;
+        t = min(t + S[cur], T[(cur + 1) % N]);
+    }
+
+    FORE(a, ans) cout << a << endl;
+
+    return 0;
+}
