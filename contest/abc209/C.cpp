df853167084018c869ecefbb426816c7476a688d
nobushi95 <nobushi1121@gmail.com>
2022-10-31 23:18:37 +0900

    add abc209/C

---
+++ contest/abc209/C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+using mint = atcoder::modint1000000007;
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> C(N);
+    FORE(c, C) cin >> c;
+    sort(ALL(C));
+
+    mint ans = 1;
+    REP(i, 0, N) ans *= C[i] - i;
+
+    cout << ans.val() << endl;
+
+    return 0;
+}
