6ae19415788c61b293e08e3c51010c655542f652
nobushi95 <nobushi1121@gmail.com>
2022-11-05 21:33:35 +0900

    add abc205/C

---
+++ contest/abc205/C.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll A, B, C;
+    cin >> A >> B >> C;
+
+    if (A >= 0 && B >= 0) {
+        if (A > B) cout << ">" << endl;
+        else if (A == B) cout << "=" << endl;
+        else cout << "<" << endl;
+    } else if (A < 0 && B >= 0) {
+        if (C % 2 == 0) {
+            if (abs(A) > abs(B)) cout << ">" << endl;
+            else if (abs(A) == abs(B)) cout << "=" << endl;
+            else cout << "<" << endl;
+        } else {
+            cout << "<" << endl;
+        }
+    } else if (A >= 0 && B < 0) {
+        if (C % 2 == 0) {
+            if (abs(A) > abs(B)) cout << ">" << endl;
+            else if (abs(A) == abs(B)) cout << "=" << endl;
+            else cout << "<" << endl;
+        } else {
+            cout << ">" << endl;
+        }
+    } else if (A < 0 && B < 0) {
+        if (C % 2 == 0) {
+            if (abs(A) > abs(B)) cout << ">" << endl;
+            else if (abs(A) == abs(B)) cout << "=" << endl;
+            else cout << "<" << endl;
+        } else {
+            if (abs(A) > abs(B)) cout << "<" << endl;
+            else if (abs(A) == abs(B)) cout << "=" << endl;
+            else cout << ">" << endl;
+        }
+    }
+
+    return 0;
+}
