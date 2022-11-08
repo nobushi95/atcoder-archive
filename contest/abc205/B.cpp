9e36c56adbb8fa17607ab92056f8e21ac0d208aa
nobushi95 <nobushi1121@gmail.com>
2022-11-08 23:26:08 +0900

    add abc205/B

---
+++ contest/abc205/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    FORE(a, A) cin >> a;
+
+    map<ll, ll> tbl;
+    FORE(a, A) tbl[a]++;
+
+    REPEQ(i, 1, N) {
+        if (tbl[i] != 1) {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+
+    cout << "Yes" << endl;
+
+    return 0;
+}
