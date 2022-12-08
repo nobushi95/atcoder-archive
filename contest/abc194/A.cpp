3651c54002ed1b8d9c6605c7bc79018122602a3a
nobushi95 <nobushi1121@gmail.com>
2022-12-09 00:09:57 +0900

    add abc194/A

---
+++ contest/abc194/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll A, B;
+    cin >> A >> B;
+
+    ll C = A + B;
+    ll ans;
+    if (C >= 15 && B >= 8) ans = 1;
+    else if (C >= 10 && B >= 3) ans = 2;
+    else if (C >= 3) ans = 3;
+    else ans = 4;
+
+    cout << ans << endl;
+
+    return 0;
+}
