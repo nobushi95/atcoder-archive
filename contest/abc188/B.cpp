36014bcd1b82c337de0f446f14a41e7ca9138550
nobushi95 <nobushi1121@gmail.com>
2022-12-23 00:38:18 +0900

    add abc188/B

---
+++ contest/abc188/B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> A(N), B(N);
+    FORE(a, A) cin >> a;
+    FORE(b, B) cin >> b;
+
+    ll prod = 0;
+    REP(i, 0, N) prod += A[i] * B[i];
+
+    if (prod == 0) cout <<"Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
