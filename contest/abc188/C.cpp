2c616fe230a7cc619e46d62843c0ccc44c315164
nobushi95 <nobushi1121@gmail.com>
2022-12-24 18:19:56 +0900

    add abc188/C

---
+++ contest/abc188/C.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> A(pow(2, N));
+    FORE(a, A) cin >> a;
+
+    ll maxIdx = distance(A.begin(), max_element(ALL(A)));
+    ll halfIdx = pow(2, N - 1);
+    auto itr = maxIdx < halfIdx ? A.begin() + halfIdx : A.begin();
+
+    ll ans = distance(A.begin(), max_element(itr, itr + halfIdx)) + 1;
+    cout << ans << endl;
+
+    return 0;
+}
