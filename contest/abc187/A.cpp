2f5b932b46d57a2b050054d815db0a44f98b4550
nobushi95 <nobushi1121@gmail.com>
2022-12-28 00:13:23 +0900

    add abc187/A

---
+++ contest/abc187/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    string A, B;
+    cin >> A >> B;
+
+    ll a = 0, b = 0;
+    REP(i, 0, 3) {
+        a += A[i] - '0';
+        b += B[i] - '0';
+    }
+
+    cout << max(a, b) << endl;
+
+    return 0;
+}
