ab403bf0837c5fad229968f1aeb8054fd8811e2c
nobushi95 <nobushi1121@gmail.com>
2023-01-10 23:32:46 +0900

    add abc181/B

---
+++ contest/abc181/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> A(N), B(N);
+    REP(i, 0, N) cin >> A[i] >> B[i];
+
+    ll ans = 0;
+    REP(i, 0, N) ans += (A[i] + B[i]) * (B[i] - A[i] + 1) / 2;
+
+    cout << ans << endl;
+
+    return 0;
+}
