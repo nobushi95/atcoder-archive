a2b496d350e16595783391d9dd967c5131caddd3
nobushi95 <nobushi1121@gmail.com>
2023-01-24 23:41:17 +0900

    add abc176/B

---
+++ contest/abc176/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    string N;
+    cin >> N;
+
+    ll sum = 0;
+    FORE(n, N) sum += n - '0';
+
+    if (sum % 9 == 0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
