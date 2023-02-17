26ca159b01e1e49e8adcfd69837a5e03e5d85dd0
nobushi95 <nobushi1121@gmail.com>
2023-02-18 01:53:33 +0900

    add abc164/B

---
+++ contest/abc164/B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll A, B, C, D;
+    cin >> A >> B >> C >> D;
+
+    while (A > 0 && C > 0) {
+        C -= B;
+        if (C <= 0) {
+            cout << "Yes" << endl;
+            break;
+        }
+        A -= D;
+        if (A <= 0) {
+            cout << "No" << endl;
+            break;
+        }
+    }
+    
+    return 0;
+}
