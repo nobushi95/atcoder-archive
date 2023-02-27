732d53a5d03de6770a83ec218c6b5ecb7231d7a4
nobushi95 <nobushi1121@gmail.com>
2023-02-27 23:34:46 +0900

    add abc162/C

---
+++ contest/abc162/C.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll K;
+    cin >> K;
+
+    ll ans = 0;
+    REPEQ(a, 1, K) REPEQ(b, 1, K) REPEQ(c, 1, K) ans += gcd(gcd(a, b), c);
+
+    cout << ans << endl;
+
+    return 0;
+}
