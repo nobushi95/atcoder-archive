63b314359a72c63bbbe8e6e59abf23789d4046bc
nobushi95 <nobushi1121@gmail.com>
2023-04-02 14:47:17 +0900

    add abc145/b

--- contest_rust/abc145/src/bin/b.rs
+++ contest_rust/abc145/src/bin/b.rs
@@ -1,3 +1,14 @@
+use proconio::{input, marker::*};
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        S: Bytes,
+    };
+    let ans = if N % 2 == 0 && (S[0..(S.len() / 2)]) == (S[(S.len() / 2)..S.len()]) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
 }
