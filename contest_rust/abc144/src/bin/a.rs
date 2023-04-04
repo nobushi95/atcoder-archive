b2e4cb1a68b21e8a0f3acfddd3bf4f68a3d8ea32
nobushi95 <nobushi1121@gmail.com>
2023-04-05 00:09:40 +0900

    add abc144/B

--- contest_rust/abc144/src/bin/a.rs
+++ contest_rust/abc144/src/bin/a.rs
@@ -1,5 +1,14 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64
+    };
+
+    let ans = match (a, b) {
+        (1..=9, 1..=9) => a * b,
+        _ => -1,
+    };
+    println!("{}", ans);
 }
