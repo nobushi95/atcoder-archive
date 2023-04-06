5dffdfc1b5d627501a2b84a5595f5da6586869ce
nobushi95 <nobushi1121@gmail.com>
2023-04-07 00:24:26 +0900

    add abc143/B

--- contest_rust/abc143/src/bin/b.rs
+++ contest_rust/abc143/src/bin/b.rs
@@ -1,5 +1,16 @@
+use itertools::Itertools;
 use proconio::{input, marker::*};
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        d: [i64; n],
+    }
+
+    let ans = d
+        .iter()
+        .tuple_combinations()
+        .map(|(x, y)| x * y)
+        .sum::<i64>();
+    println!("{}", ans);
 }
