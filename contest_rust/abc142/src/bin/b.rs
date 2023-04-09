50897fc532f62f2c77641b59e07ff60ce1a472dd
nobushi95 <nobushi1121@gmail.com>
2023-04-09 22:07:43 +0900

    add abc142/B

--- contest_rust/abc142/src/bin/b.rs
+++ contest_rust/abc142/src/bin/b.rs
@@ -1,5 +1,11 @@
 use proconio::{input, marker::*};
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        k: i64,
+        h: [i64; n]
+    };
+    let ans = h.iter().filter(|&x| *x >= k).count();
+    println!("{}", ans);
 }
