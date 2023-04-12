e8cf86274284fb3281380c0403aae34cf97e7d38
nobushi95 <nobushi1121@gmail.com>
2023-04-13 00:36:09 +0900

    add abc141/A

--- contest_rust/abc141/src/bin/a.rs
+++ contest_rust/abc141/src/bin/a.rs
@@ -3,5 +3,12 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    let weathers = vec!["Sunny", "Cloudy", "Rainy"];
+    input! {
+        s: String
+    };
+
+    let idx = weathers.iter().position(|&x| x.to_string() == s).unwrap();
+    let ans = weathers[(idx + 1) % 3];
+    println!("{}", ans);
 }
