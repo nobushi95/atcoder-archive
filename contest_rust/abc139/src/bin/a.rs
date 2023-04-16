5a6958a69588a38df8b1bd33b7e69a08dd22115c
nobushi95 <nobushi1121@gmail.com>
2023-04-16 14:25:07 +0900

    add abc139/A

--- contest_rust/abc139/src/bin/a.rs
+++ contest_rust/abc139/src/bin/a.rs
@@ -1,7 +1,14 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
+use regex::internal::Char;
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    };
+
+    let ans = s.iter().zip(t.iter()).filter(|(s, t)| s == t).count();
+    println!("{}", ans);
 }
