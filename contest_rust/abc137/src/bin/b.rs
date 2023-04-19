bcbe5d115b47cf5bf3c071777b50e12afcf1b2f0
nobushi95 <nobushi1121@gmail.com>
2023-04-20 00:57:35 +0900

    add abc137/B

--- contest_rust/abc137/src/bin/b.rs
+++ contest_rust/abc137/src/bin/b.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        k: i64,
+        x: i64,
+    };
+
+    let l = max(x - k + 1, -1000000);
+    let r = min(x + k - 1, 1000000);
+    let ans = (l..=r).join(" ");
+    println!("{}", ans);
 }
