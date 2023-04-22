698362e76eb097959a45dd5f300e442a1cf86d94
nobushi95 <nobushi1121@gmail.com>
2023-04-22 17:01:34 +0900

    add abc136/B

--- contest_rust/abc136/src/bin/b.rs
+++ contest_rust/abc136/src/bin/b.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+
+    let ans = (1..=n).filter(|x| x.to_string().len() % 2 != 0).count();
+    println!("{}", ans);
 }
