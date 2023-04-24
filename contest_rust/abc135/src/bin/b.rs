dc9ebfb2866b8382a700cc4ebb139fd3efdc236b
nobushi95 <nobushi1121@gmail.com>
2023-04-24 23:53:08 +0900

    add abc135/B

--- contest_rust/abc135/src/bin/b.rs
+++ contest_rust/abc135/src/bin/b.rs
@@ -3,5 +3,21 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        p: [i64; n],
+    };
+
+    let wrong = p
+        .iter()
+        .enumerate()
+        .filter(|(i, &val)| i + 1 != val as usize)
+        .count();
+
+    let ans = if wrong == 0 || wrong == 2 {
+        "YES"
+    } else {
+        "NO"
+    };
+    println!("{}", ans);
 }
