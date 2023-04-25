73a1a4ca9840cd6fcd660cb3235860226fd9febf
nobushi95 <nobushi1121@gmail.com>
2023-04-25 22:21:05 +0900

    add abc135/A

--- contest_rust/abc135/src/bin/a.rs
+++ contest_rust/abc135/src/bin/a.rs
@@ -1,7 +1,18 @@
 use itertools::Itertools;
+use num::abs_sub;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+    };
+    let diff = (a - b).abs();
+    let ans = if diff % 2 == 0 {
+        (diff / 2 + min(a, b)).to_string()
+    } else {
+        String::from("IMPOSSIBLE")
+    };
+    println!("{}", ans)
 }
