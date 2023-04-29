ed0a59d33ad97df4832cfa94fd30eb4fa0fa830d
nobushi95 <nobushi1121@gmail.com>
2023-04-29 17:20:30 +0900

    add abc132/B

--- contest_rust/abc132/src/bin/b.rs
+++ contest_rust/abc132/src/bin/b.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: [i64; n],
+    };
+    let ans = p
+        .windows(3)
+        .filter(|x| (x[0] < x[1] && x[1] < x[2] || (x[2] < x[1] && x[1] < x[0])))
+        .count();
+    println!("{}", ans);
 }
