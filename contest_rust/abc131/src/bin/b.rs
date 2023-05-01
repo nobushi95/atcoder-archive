2a6e4b47fdb7b2bcac6e9bbc41b705c28f7c349f
nobushi95 <nobushi1121@gmail.com>
2023-05-01 23:32:08 +0900

    add abc131/B

--- contest_rust/abc131/src/bin/b.rs
+++ contest_rust/abc131/src/bin/b.rs
@@ -1,7 +1,14 @@
 use itertools::Itertools;
+use num::Signed;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        l: i64,
+    };
+    let sum = (l..(l + n)).sum::<i64>();
+    let ans = sum - (l..(l + n)).min_by(|a, b| a.abs().cmp(&b.abs())).unwrap();
+    println!("{}", ans);
 }
