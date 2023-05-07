ee9e8e6e7690446652bb2eb7873f4b57c792985b
nobushi95 <nobushi1121@gmail.com>
2023-05-07 14:57:50 +0900

    add abc130/B

--- contest_rust/abc130/src/bin/b.rs
+++ contest_rust/abc130/src/bin/b.rs
@@ -3,5 +3,19 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        x: i64,
+        l: [i64; n],
+    };
+    let ans = l
+        .iter()
+        .scan(0, |cur, y| {
+            *cur += y;
+            Some(*cur)
+        })
+        .filter(|&y| y <= x)
+        .count()
+        + 1;
+    println!("{}", ans);
 }
