45bd823a437bfcd048b2117ec56e4d2d3296df8a
nobushi95 <nobushi1121@gmail.com>
2023-05-08 23:46:23 +0900

    add abc130/A

--- contest_rust/abc130/src/bin/a.rs
+++ contest_rust/abc130/src/bin/a.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        x: i64,
+        a: i64,
+    };
+    let ans = if x < a { 0 } else { 10 };
+    println!("{}", ans);
 }
