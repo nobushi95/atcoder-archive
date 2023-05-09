5773c39e7db4df7dcf2fb44d055c1b7ed971bbdf
nobushi95 <nobushi1121@gmail.com>
2023-05-10 00:36:41 +0900

    add abc129/B

--- contest_rust/abc129/src/bin/b.rs
+++ contest_rust/abc129/src/bin/b.rs
@@ -1,7 +1,21 @@
 use itertools::Itertools;
+use num_traits::abs_sub;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        w: [i64; n]
+    };
+    let sum = w.iter().sum::<i64>();
+    let mut acc = w
+        .iter()
+        .scan(0, |cur, x| {
+            *cur += x;
+            Some(*cur)
+        })
+        .collect::<Vec<_>>();
+    let ans = acc.iter().map(|x| (sum - 2 * x).abs()).min().unwrap();
+    println!("{}", ans);
 }
