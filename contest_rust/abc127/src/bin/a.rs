57fc1afc69cebfd404e2e91f6b8592fcb3d23c0c
nobushi95 <nobushi1121@gmail.com>
2023-05-14 19:15:03 +0900

    add abc127/A

--- contest_rust/abc127/src/bin/a.rs
+++ contest_rust/abc127/src/bin/a.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+    };
+    let ans = match a {
+        x if x >= 13 => b,
+        6..=12 => b / 2,
+        _ => 0,
+    };
+    println!("{}", ans);
 }
