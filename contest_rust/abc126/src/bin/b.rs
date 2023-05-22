00a19e4497203ee33313cbf977c701e2b3e0f99b
nobushi95 <nobushi1121@gmail.com>
2023-05-22 23:25:25 +0900

    add abc126/B

--- contest_rust/abc126/src/bin/b.rs
+++ contest_rust/abc126/src/bin/b.rs
@@ -3,5 +3,16 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: String
+    };
+    let upper = s[..2].parse::<i64>().unwrap();
+    let lower = s[2..].parse::<i64>().unwrap();
+    let ans = match (upper, lower) {
+        (1..=12, 1..=12) => "AMBIGUOUS",
+        (1..=12, _) => "MMYY",
+        (_, 1..=12) => "YYMM",
+        _ => "NA",
+    };
+    println!("{}", ans);
 }
