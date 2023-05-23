5fcfc8fd75e64520c116901829be510ec35f8a43
nobushi95 <nobushi1121@gmail.com>
2023-05-23 23:16:10 +0900

    add abc126/A

--- contest_rust/abc126/src/bin/a.rs
+++ contest_rust/abc126/src/bin/a.rs
@@ -3,5 +3,11 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        mut s: Chars,
+    };
+    s[k - 1] = s[k - 1].to_ascii_lowercase();
+    println!("{}", s.iter().join(""));
 }
