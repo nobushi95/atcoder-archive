f94bb3ef123a5801d80fa9a4fa3a16a68020f473
nobushi95 <nobushi1121@gmail.com>
2023-05-28 13:45:11 +0900

    add abc124/B

--- contest_rust/abc124/src/bin/b.rs
+++ contest_rust/abc124/src/bin/b.rs
@@ -3,5 +3,21 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        h: [i64; n],
+    };
+    let ans = h
+        .iter()
+        .scan(0, |current_max, &x| {
+            if x >= *current_max {
+                *current_max = x;
+                Some(true)
+            } else {
+                Some(false)
+            }
+        })
+        .filter(|&x| x)
+        .count();
+    println!("{:?}", ans);
 }
