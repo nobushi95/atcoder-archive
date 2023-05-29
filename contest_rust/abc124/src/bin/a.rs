9eb5ca30ab41cf80da22f930febc4038f22f4df3
nobushi95 <nobushi1121@gmail.com>
2023-05-29 23:41:32 +0900

    add abc124/A

--- contest_rust/abc124/src/bin/a.rs
+++ contest_rust/abc124/src/bin/a.rs
@@ -3,5 +3,24 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        mut a: i64,
+        mut b: i64,
+    };
+
+    let chose = |a: &mut i64, b: &mut i64| {
+        if a > b {
+            let tmp = *a;
+            *a -= 1;
+            tmp
+        } else {
+            let tmp = *b;
+            *b -= 1;
+            tmp
+        }
+    };
+
+    let mut ans = chose(&mut a, &mut b);
+    ans += chose(&mut a, &mut b);
+    println!("{}", ans);
 }
