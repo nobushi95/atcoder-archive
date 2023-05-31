3a66c8795e904d3e5f8d1dc78dcb4bdd8c01442d
nobushi95 <nobushi1121@gmail.com>
2023-05-31 23:31:16 +0900

    add abc122/A

--- contest_rust/abc122/src/bin/a.rs
+++ contest_rust/abc122/src/bin/a.rs
@@ -3,5 +3,19 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        b: Chars
+    };
+
+    let ans = b
+        .iter()
+        .map(|&x| match x {
+            'A' => 'T',
+            'T' => 'A',
+            'C' => 'G',
+            'G' => 'C',
+            _ => unreachable!(),
+        })
+        .join("");
+    println!("{}", ans);
 }
