095f44768f0cd4050dd9ca57a997fa10bc33f7cd
nobushi95 <nobushi1121@gmail.com>
2023-06-02 00:22:18 +0900

    add abc120/B

--- contest_rust/abc120/src/bin/b.rs
+++ contest_rust/abc120/src/bin/b.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        k: usize,
+    };
+    let ans = (1..=100)
+        .rev()
+        .filter(|&x| a % x == 0 && b % x == 0)
+        .collect_vec()[k - 1];
+    println!("{}", ans);
 }
