023d9b7eb1e1333be95360fcf02bf3cf2cf8eb23
nobushi95 <nobushi1121@gmail.com>
2023-06-03 15:43:48 +0900

    add abc120/A

--- contest_rust/abc120/src/bin/a.rs
+++ contest_rust/abc120/src/bin/a.rs
@@ -3,5 +3,11 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+    };
+    let ans = min(b / a, c);
+    println!("{}", ans);
 }
