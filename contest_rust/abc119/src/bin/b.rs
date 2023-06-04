8761f04122c15ac6d37d580ae5b41380bbac5d81
nobushi95 <nobushi1121@gmail.com>
2023-06-04 12:19:26 +0900

    add abc119/B

--- contest_rust/abc119/src/bin/b.rs
+++ contest_rust/abc119/src/bin/b.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        xu: [(f64, String); n],
+    };
+    let ans = xu
+        .iter()
+        .map(|(x, u)| if u == "JPY" { *x } else { *x * 380000_f64 })
+        .sum::<f64>();
+    println!("{}", ans);
 }
