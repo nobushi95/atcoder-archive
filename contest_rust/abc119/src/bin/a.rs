b01fb2c232c007fe285557cf901b99f000ad2366
nobushi95 <nobushi1121@gmail.com>
2023-06-05 23:04:22 +0900

    add abc119/A

--- contest_rust/abc119/src/bin/a.rs
+++ contest_rust/abc119/src/bin/a.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    };
+    let ans = if s <= String::from("2019/04/30") {
+        "Heisei"
+    } else {
+        "TBD"
+    };
+    println!("{}", ans);
 }
