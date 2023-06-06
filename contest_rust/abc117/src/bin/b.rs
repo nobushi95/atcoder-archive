37a3988a007a75716f4d26e064c728b094223818
nobushi95 <nobushi1121@gmail.com>
2023-06-07 00:08:35 +0900

    add abc117/B

--- contest_rust/abc117/src/bin/b.rs
+++ contest_rust/abc117/src/bin/b.rs
@@ -3,5 +3,15 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        l: [i64; n],
+    };
+    let &max_length = l.iter().max().unwrap();
+    let ans = if (l.iter().sum::<i64>() - max_length) > max_length {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
 }
