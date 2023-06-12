a5085dc0d6f6cb332b963feeb0c7eadb327f38a9
nobushi95 <nobushi1121@gmail.com>
2023-06-13 00:32:50 +0900

    add abc114/A

--- contest_rust/abc114/src/bin/a.rs
+++ contest_rust/abc114/src/bin/a.rs
@@ -3,5 +3,12 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        x: i64,
+    };
+    let ans = match x {
+        7 | 5 | 3 => "YES",
+        _ => "NO",
+    };
+    println!("{}", ans);
 }
