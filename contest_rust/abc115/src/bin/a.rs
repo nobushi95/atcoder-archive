623ee6e50d9b12dbe275c98460a099fa27a9a9e9
nobushi95 <nobushi1121@gmail.com>
2023-06-12 23:42:14 +0900

    add abc115/A

--- contest_rust/abc115/src/bin/a.rs
+++ contest_rust/abc115/src/bin/a.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! { d: i64 };
+    let ans = match d {
+        22 => "Christmas Eve Eve Eve",
+        23 => "Christmas Eve Eve",
+        24 => "Christmas Eve",
+        25 => "Christmas",
+        _ => unreachable!(),
+    };
+    println! {"{}", ans};
 }
