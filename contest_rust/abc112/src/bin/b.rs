fa9c0d2474472a97fe0672637c318cad4f12f7e6
nobushi95 <nobushi1121@gmail.com>
2023-06-17 14:57:05 +0900

    add abc112/B

--- contest_rust/abc112/src/bin/b.rs
+++ contest_rust/abc112/src/bin/b.rs
@@ -3,5 +3,19 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        t: i64,
+        ct: [(i64, i64); n],
+    };
+    let ans = match ct
+        .iter()
+        .filter(|(_ci, ti)| *ti <= t)
+        .min_by_key(|(ci, _ti)| *ci)
+    {
+        Some((ci, _ti)) => ci.to_string(),
+        None => String::from("TLE"),
+    };
+
+    println!("{}", ans);
 }
