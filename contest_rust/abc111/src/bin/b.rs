a129a1c88a594b60ff2517156fae85cf3149b463
nobushi95 <nobushi1121@gmail.com>
2023-06-19 23:21:03 +0900

    add abc111/B

--- contest_rust/abc111/src/bin/b.rs
+++ contest_rust/abc111/src/bin/b.rs
@@ -1,7 +1,12 @@
 use itertools::Itertools;
+use num_traits::abs_sub;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+    let ans = (n..=999).filter(|x| x % 111 == 0).next().unwrap();
+    println!("{}", ans);
 }
