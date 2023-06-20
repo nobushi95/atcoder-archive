b9f46c84d69bd274d7219e39aecdb5d5dea05bd0
nobushi95 <nobushi1121@gmail.com>
2023-06-20 23:13:55 +0900

    add abc111/A

--- contest_rust/abc111/src/bin/a.rs
+++ contest_rust/abc111/src/bin/a.rs
@@ -3,5 +3,16 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: Chars
+    };
+    let ans = n
+        .iter()
+        .map(|c| match *c {
+            '1' => '9',
+            '9' => '1',
+            _ => unreachable!(),
+        })
+        .join("");
+    println!("{}", ans);
 }
