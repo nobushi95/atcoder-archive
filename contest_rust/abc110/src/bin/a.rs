971ec8c9a8d11893503e86c89523cbcd71080b04
nobushi95 <nobushi1121@gmail.com>
2023-06-22 00:01:55 +0900

    add abc110/A

--- contest_rust/abc110/src/bin/a.rs
+++ contest_rust/abc110/src/bin/a.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        mut n: [i64; 3]
+    };
+    n.sort();
+    let ans = n[0] + n[1] + n[2] * 10;
+    println!("{}", ans);
 }
