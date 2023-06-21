439f24824456de739d5aca568441643ecac49051
nobushi95 <nobushi1121@gmail.com>
2023-06-21 23:46:40 +0900

    add abc110/B

--- contest_rust/abc110/src/bin/b.rs
+++ contest_rust/abc110/src/bin/b.rs
@@ -3,5 +3,21 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        m: i64,
+        xx: i64,
+        yy: i64,
+        x: [i64; n],
+        y: [i64; m],
+    };
+    let &xmax = x.iter().max().unwrap();
+    let &ymin = y.iter().min().unwrap();
+    let diff = ymin - xmax;
+    let ans = if diff > 0 && xx < xmax && ymin < yy {
+        "No War"
+    } else {
+        "War"
+    };
+    println!("{}", ans);
 }
