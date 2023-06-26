cbba20777a6c2c0a610e4c2596295985fafb5f3b
nobushi95 <nobushi1121@gmail.com>
2023-06-26 23:43:29 +0900

    add abc106/A

--- contest_rust/abc106/src/bin/a.rs
+++ contest_rust/abc106/src/bin/a.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+    };
+    let ans = (a - 1) * (b - 1);
+    println!("{}", ans);
 }
