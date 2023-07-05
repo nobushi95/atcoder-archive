29a6d07228c2a79ad620dbf4bc469bc723d5a017
nobushi95 <nobushi1121@gmail.com>
2023-07-05 23:36:02 +0900

    add abc101/B

--- contest_rust/abc101/src/bin/b.rs
+++ contest_rust/abc101/src/bin/b.rs
@@ -3,5 +3,18 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: Chars,
+    };
+    let ans = if n.iter().join("").parse::<i64>().unwrap()
+        % n.iter()
+            .map(|&x| x.to_digit(10).unwrap() as i64)
+            .sum::<i64>()
+        == 0
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
 }
