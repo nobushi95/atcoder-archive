283f5db3ec6f28979b2e3ceadb30b3d2db116ed7
nobushi95 <nobushi1121@gmail.com>
2023-07-11 23:11:57 +0900

    add abc096/B

--- contest_rust/abc096/src/bin/b.rs
+++ contest_rust/abc096/src/bin/b.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+        k: i64,
+    };
+    let bind = [a, b, c];
+    let ans = bind.iter().max().unwrap() * (2_i64.pow(k as u32) - 1) + bind.iter().sum::<i64>();
+    println!("{}", ans);
 }
