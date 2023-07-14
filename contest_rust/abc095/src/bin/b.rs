205dcda73a4a0f8b4145e175447bacca526a05e3
nobushi95 <nobushi1121@gmail.com>
2023-07-14 16:57:45 +0900

    add abc095/B

--- contest_rust/abc095/src/bin/b.rs
+++ contest_rust/abc095/src/bin/b.rs
@@ -3,5 +3,12 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        mut x: i64,
+        m: [i64; n],
+    };
+    x -= m.iter().sum::<i64>();
+    let ans = n + x / m.iter().min().unwrap();
+    println!("{}", ans);
 }
