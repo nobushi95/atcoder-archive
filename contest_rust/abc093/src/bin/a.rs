494e15a69f829c26785d294d39ba5b43d2c217bb
nobushi95 <nobushi1121@gmail.com>
2023-07-18 17:49:58 +0900

    add abc093/A

--- contest_rust/abc093/src/bin/a.rs
+++ contest_rust/abc093/src/bin/a.rs
@@ -1,7 +1,19 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, iter::FromIterator};
 
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    };
+    let ans = if s
+        .into_iter()
+        .permutations(3)
+        .any(|x| x.iter().collect::<String>() == "abc")
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
 }
