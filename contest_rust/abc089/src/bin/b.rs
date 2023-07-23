707a95c35f9e8f71b5b395a0d1c56765501471eb
nobushi95 <nobushi1121@gmail.com>
2023-07-23 22:01:54 +0900

    add abc089/B

--- contest_rust/abc089/src/bin/b.rs
+++ contest_rust/abc089/src/bin/b.rs
@@ -1,7 +1,17 @@
+use im_rc::HashSet;
 use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [char; n],
+    };
+    let ans = if s.into_iter().collect::<HashSet<char>>().len() == 3 {
+        "Three"
+    } else {
+        "Four"
+    };
+    println!("{}", ans);
 }
