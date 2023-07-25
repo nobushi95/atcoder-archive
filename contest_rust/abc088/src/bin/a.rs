9179c070e14058c96448a6dcb30dc0182cacab19
nobushi95 <nobushi1121@gmail.com>
2023-07-25 23:43:19 +0900

    add abc088/A

--- contest_rust/abc088/src/bin/a.rs
+++ contest_rust/abc088/src/bin/a.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        a: i64,
+    };
+    let ans = if n % 500 <= a { "Yes" } else { "No" };
+    println!("{}", ans);
 }
