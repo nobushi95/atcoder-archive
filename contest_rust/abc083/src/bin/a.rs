085b10597f9ba9b643d6c73d3c81574619b5c86d
nobushi95 <nobushi1121@gmail.com>
2023-07-30 14:06:11 +0900

    add abc083/A

--- contest_rust/abc083/src/bin/a.rs
+++ contest_rust/abc083/src/bin/a.rs
@@ -3,5 +3,20 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+        d: i64
+    };
+    let left = a + b;
+    let right = c + d;
+    let ans = if left > right {
+        "Left"
+    } else if left == right {
+        "Balanced"
+    } else {
+        "Right"
+    };
+    println!("{}", ans);
 }
