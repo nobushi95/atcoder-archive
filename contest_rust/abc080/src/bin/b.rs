01c741db73b8207de3c3466ec137c695425f9c59
nobushi95 <nobushi1121@gmail.com>
2023-08-01 23:02:57 +0900

    add abc080/B

--- contest_rust/abc080/src/bin/b.rs
+++ contest_rust/abc080/src/bin/b.rs
@@ -2,6 +2,19 @@ use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
+fn f(mut x: i64) -> i64 {
+    let mut ans = 0;
+    while x > 0 {
+        ans += x % 10;
+        x /= 10;
+    }
+    ans
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+    let ans = if n % f(n) == 0 { "Yes" } else { "No" };
+    println!("{}", ans);
 }
