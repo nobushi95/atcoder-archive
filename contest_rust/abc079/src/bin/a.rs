b08ee4b2cfc5a71942ba5fa4d1fdc410c0df8132
nobushi95 <nobushi1121@gmail.com>
2023-08-03 23:49:33 +0900

    add abc079/A

--- contest_rust/abc079/src/bin/a.rs
+++ contest_rust/abc079/src/bin/a.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: Chars
+    };
+    let ans = if n.windows(3).any(|x| x[0] == x[1] && x[0] == x[2]) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans);
 }
