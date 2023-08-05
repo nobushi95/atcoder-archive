9e893550508f4953a8ce488c49653a2062fca905
nobushi95 <nobushi1121@gmail.com>
2023-08-05 22:46:57 +0900

    add abc078/B

--- contest_rust/abc078/src/bin/b.rs
+++ contest_rust/abc078/src/bin/b.rs
@@ -3,5 +3,15 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        x: i64,
+        y: i64,
+        z: i64,
+    };
+    let ans = if x % (y + z) >= z {
+        x / (y + z)
+    } else {
+        x / (y + z) - 1
+    };
+    println!("{}", ans);
 }
