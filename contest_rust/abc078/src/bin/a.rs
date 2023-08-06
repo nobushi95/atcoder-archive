47e101446a08353c8efa57762fa94ccc4adb6f38
nobushi95 <nobushi1121@gmail.com>
2023-08-06 16:29:07 +0900

    add abc078/A

--- contest_rust/abc078/src/bin/a.rs
+++ contest_rust/abc078/src/bin/a.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        x: char,
+        y: char,
+    };
+    let ans = match x.cmp(&y) {
+        Ordering::Equal => "=",
+        Ordering::Greater => ">",
+        Ordering::Less => "<",
+    };
+    println!("{}", ans);
 }
