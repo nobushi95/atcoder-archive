e49aa9d9cc2830da7d2de068be328cf97f9c6184
nobushi95 <nobushi1121@gmail.com>
2023-08-07 22:59:23 +0900

    add abc077/B

--- contest_rust/abc077/src/bin/b.rs
+++ contest_rust/abc077/src/bin/b.rs
@@ -1,7 +1,12 @@
 use itertools::Itertools;
+use num_integer::Roots;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+    let ans = n.sqrt().pow(2);
+    println!("{}", ans);
 }
