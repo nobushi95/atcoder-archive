adbeffea07420d97b0b9786d27ec1a3ca967c4c2
nobushi95 <nobushi1121@gmail.com>
2023-08-08 23:51:28 +0900

    add abc077/A

--- contest_rust/abc077/src/bin/a.rs
+++ contest_rust/abc077/src/bin/a.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        c: [Chars; 2],
+    };
+    let ans = if c[0][1] == c[1][1] && c[0][0] == c[1][2] && c[0][2] == c[1][0] {
+        "YES"
+    } else {
+        "NO"
+    };
+    println!("{}", ans);
 }
