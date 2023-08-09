72b2bd6a34a327228a1847b13c2b4c3df16424d1
nobushi95 <nobushi1121@gmail.com>
2023-08-09 23:41:53 +0900

    add abc076/B

--- contest_rust/abc076/src/bin/b.rs
+++ contest_rust/abc076/src/bin/b.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        k: i64,
+    };
+    let ans = (0..n).fold(1, |acc, _| (acc * 2).min(acc + k));
+    println!("{}", ans);
 }
