1dcadcfc3f4b32bc469a76fe13e7469789f7ce6f
nobushi95 <nobushi1121@gmail.com>
2023-08-26 13:42:00 +0900

    add abc067/B

--- contest_rust/abc067/src/bin/b.rs
+++ contest_rust/abc067/src/bin/b.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input![
+        n: i64,
+        k: i64,
+        mut l: [i64; n],
+    ];
+
+    l.sort();
+    l.reverse();
+    let ans = (0..k as usize).map(|i| l[i]).sum::<i64>();
+    println!("{}", ans);
 }
