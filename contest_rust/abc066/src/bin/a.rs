b472d1bbdc22481cb32965e922928602ad27d4e0
nobushi95 <nobushi1121@gmail.com>
2023-08-28 23:54:10 +0900

    add abc066/A

--- contest_rust/abc066/src/bin/a.rs
+++ contest_rust/abc066/src/bin/a.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+    };
+    let mut v = vec![a, b, c];
+    v.sort();
+    let ans = v[0] + v[1];
+    println!("{}", ans);
 }
