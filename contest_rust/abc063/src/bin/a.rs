556166b3083851e1d8419dc32e7626a25735a40c
nobushi95 <nobushi1121@gmail.com>
2023-09-01 22:07:34 +0900

    add abc063/A

--- contest_rust/abc063/src/bin/a.rs
+++ contest_rust/abc063/src/bin/a.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+    };
+    let ans = if a + b >= 10 {
+        String::from("error")
+    } else {
+        (a + b).to_string()
+    };
+    println!("{}", ans);
 }
