66a1b86c1119674bf1d989d7b7e760df884a582e
nobushi95 <nobushi1121@gmail.com>
2023-09-04 23:44:35 +0900

    add abc060/A

--- contest_rust/abc060/src/bin/a.rs
+++ contest_rust/abc060/src/bin/a.rs
@@ -3,5 +3,16 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: Chars,
+        b: Chars,
+        c: Chars,
+    };
+    let ans = if a.last().unwrap() == b.first().unwrap() && b.last().unwrap() == c.first().unwrap()
+    {
+        "YES"
+    } else {
+        "NO"
+    };
+    println!("{}", ans);
 }
