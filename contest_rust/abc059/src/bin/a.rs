5c84987c6a28a2e455ed7f0710c05c39bcca1ab3
nobushi95 <nobushi1121@gmail.com>
2023-09-05 23:15:12 +0900

    add abc059/A

--- contest_rust/abc059/src/bin/a.rs
+++ contest_rust/abc059/src/bin/a.rs
@@ -3,5 +3,16 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s1: Chars,
+        s2: Chars,
+        s3: Chars,
+    };
+    let ans = format!(
+        "{}{}{}",
+        s1.first().unwrap().to_uppercase(),
+        s2.first().unwrap().to_uppercase(),
+        s3.first().unwrap().to_uppercase()
+    );
+    println!("{}", ans);
 }
