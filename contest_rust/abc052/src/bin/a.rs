0ff0ed6695e45cd3a48cf80611c7ce61883064c2
nobushi95 <nobushi1121@gmail.com>
2023-09-14 23:22:57 +0900

    add abc052/A

--- contest_rust/abc052/src/bin/a.rs
+++ contest_rust/abc052/src/bin/a.rs
@@ -3,5 +3,12 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+        d: i64
+    };
+    let ans = (a * b).max(c * d);
+    println!("{}", ans);
 }
