a5808012c6ff960a294c646cf211cf0370ad9454
nobushi95 <nobushi1121@gmail.com>
2023-09-19 23:34:52 +0900

    add abc047/A

--- contest_rust/abc047/src/bin/a.rs
+++ contest_rust/abc047/src/bin/a.rs
@@ -3,5 +3,11 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+    };
+    let ans = if a + b == c || a + c == b || b + c == a { "Yes" } else { "No" };
+    println!("{}", ans);
 }
