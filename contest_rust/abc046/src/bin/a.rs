47dc23c3f1f2641cb4174125254e530fff81293c
nobushi95 <nobushi1121@gmail.com>
2023-09-20 23:17:25 +0900

    add abc046/A

--- contest_rust/abc046/src/bin/a.rs
+++ contest_rust/abc046/src/bin/a.rs
@@ -1,7 +1,14 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+    };
+    let set = HashSet::from([a, b, c]);
+    let ans = set.len();
+    println!("{}", ans);
 }
