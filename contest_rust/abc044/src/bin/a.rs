8cca6190f253534a09f11a371395601ad79fc721
nobushi95 <nobushi1121@gmail.com>
2023-09-22 23:16:45 +0900

    add abc044/A

--- contest_rust/abc044/src/bin/a.rs
+++ contest_rust/abc044/src/bin/a.rs
@@ -3,5 +3,12 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        k: i64,
+        x: i64,
+        y: i64,
+    };
+    let ans = x * n.min(k) + y * (n - k).max(0);
+    println!("{}", ans);
 }
