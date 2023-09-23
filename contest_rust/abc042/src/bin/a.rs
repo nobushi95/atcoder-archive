2c6b3a7be7013cef5106cdf663305afb2f3ef9c1
nobushi95 <nobushi1121@gmail.com>
2023-09-24 00:56:38 +0900

    add abc042/A

--- contest_rust/abc042/src/bin/a.rs
+++ contest_rust/abc042/src/bin/a.rs
@@ -3,5 +3,15 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: [i64; 3],
+    };
+    let ans = if n.iter().filter(|&x| *x == 5).count() == 2
+        && n.iter().filter(|&x| *x == 7).count() == 1
+    {
+        "YES"
+    } else {
+        "NO"
+    };
+    println!("{}", ans);
 }
