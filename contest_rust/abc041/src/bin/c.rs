1d66515be9ea7641fe111518464f97a4643c7510
nobushi95 <nobushi1121@gmail.com>
2023-09-25 23:48:26 +0900

    add abc041/C

--- contest_rust/abc041/src/bin/c.rs
+++ contest_rust/abc041/src/bin/c.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        a: [i64; n],
+    };
+    let mut pairs = a.iter().enumerate().collect::<Vec<(usize, &i64)>>();
+    pairs.sort_by(|l, r| r.1.cmp(l.1));
+    for (i, _) in pairs {
+        println!("{}", i + 1);
+    }
 }
