cc5c250905067a77f517f8da928eba4b9379129f
nobushi95 <nobushi1121@gmail.com>
2023-09-26 23:26:28 +0900

    add abc041/B

--- contest_rust/abc041/src/bin/b.rs
+++ contest_rust/abc041/src/bin/b.rs
@@ -2,6 +2,14 @@ use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
+const MOD: usize = 1_000_000_007;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    };
+    let ans = a * b % MOD * c % MOD;
+    println!("{}", ans);
 }
