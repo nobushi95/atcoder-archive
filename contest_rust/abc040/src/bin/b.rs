ba37700b933773edf3de7b361632683abfc2f00e
nobushi95 <nobushi1121@gmail.com>
2023-09-29 21:55:15 +0900

    add abc040/B

--- contest_rust/abc040/src/bin/b.rs
+++ contest_rust/abc040/src/bin/b.rs
@@ -1,7 +1,13 @@
-use itertools::Itertools;
-use proconio::{input, marker::*};
-use std::cmp::*;
+use num_integer::Roots;
+use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+    let ans = (1..=n.sqrt())
+        .map(|x| ((n / x) * x - n).abs() + ((n / x) - x).abs())
+        .min()
+        .unwrap();
+    println!("{}", ans);
 }
