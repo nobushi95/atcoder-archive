b9592c85c788c2299cbd5ca6d8950a4a213d2bfb
nobushi95 <nobushi1121@gmail.com>
2023-10-07 22:37:26 +0900

    add abc037/B

--- contest_rust/abc037/src/bin/b.rs
+++ contest_rust/abc037/src/bin/b.rs
@@ -3,5 +3,12 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: i64,
+        lrt: [(usize, usize, i64); q],
+    }
+    let mut ans = vec![0_i64; n];
+    lrt.iter().for_each(|(l, r, t)| ans[(l - 1)..*r].fill(*t));
+    ans.iter().for_each(|x| println!("{}", x));
 }
