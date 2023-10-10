5aa98c893dee4ab40bf44bbb5c525fc30f714375
nobushi95 <nobushi1121@gmail.com>
2023-10-10 23:44:23 +0900

    add abc036/B

--- contest_rust/abc036/src/bin/b.rs
+++ contest_rust/abc036/src/bin/b.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [Chars; n],
+    }
+    for j in 0..n {
+        for i in (0..n).rev() {
+            print!("{}", s[i][j]);
+        }
+        println!();
+    }
 }
