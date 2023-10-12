b5a5e7bfe23511d96e2225b441a98823a1246c82
nobushi95 <nobushi1121@gmail.com>
2023-10-12 23:37:35 +0900

    add abc035/B

--- contest_rust/abc035/src/bin/b.rs
+++ contest_rust/abc035/src/bin/b.rs
@@ -3,5 +3,33 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: i64,
+    };
+    let x_abs = s
+        .iter()
+        .map(|x| match x {
+            'L' => -1,
+            'R' => 1,
+            _ => 0,
+        })
+        .sum::<i64>()
+        .abs();
+    let y_abs = s
+        .iter()
+        .map(|x| match x {
+            'U' => 1,
+            'D' => -1,
+            _ => 0,
+        })
+        .sum::<i64>()
+        .abs();
+    let k = s.iter().filter(|&x| *x == '?').count() as i64;
+    let ans = if t == 1 {
+        x_abs + y_abs + k
+    } else {
+        (s.len() as i64 % 2).max(x_abs + y_abs - k)
+    };
+    println!("{}", ans);
 }
