0e00dfc7a9259d07ec0840b19851d52ec789a387
nobushi95 <nobushi1121@gmail.com>
2023-10-15 22:48:21 +0900

    add abc035/A

--- contest_rust/abc035/src/bin/a.rs
+++ contest_rust/abc035/src/bin/a.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        w: i64,
+        h: i64,
+    };
+    let ans = if w / 4 * 3 == h { "4:3" } else { "16:9" };
+    println!("{}", ans);
 }
