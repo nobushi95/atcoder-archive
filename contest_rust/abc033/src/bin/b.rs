ec2e2ebdc98b695aea28c3a1151cc509b03cd00a
nobushi95 <nobushi1121@gmail.com>
2023-10-18 23:41:13 +0900

    add abc033/B

--- contest_rust/abc033/src/bin/b.rs
+++ contest_rust/abc033/src/bin/b.rs
@@ -1,7 +1,16 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, string};
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        sp: [(String, i64); n],
+    };
+    let sum = sp.iter().map(|x| x.1).sum::<i64>();
+    let ans = match sp.iter().filter(|(s, p)| *p > sum / 2).next() {
+        Some(n) => n.0.clone(),
+        None => String::from("atcoder"),
+    };
+    println!("{}", ans);
 }
