78550d3ca81556a55f65701b949f0912f6ddd4f8
nobushi95 <nobushi1121@gmail.com>
2023-10-19 23:25:16 +0900

    add abc033/A

--- contest_rust/abc033/src/bin/a.rs
+++ contest_rust/abc033/src/bin/a.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: Chars,
+    };
+    let ans = if n.iter().all(|c| c == n.first().unwrap()) {
+        "SAME"
+    } else {
+        "DIFFERENT"
+    };
+    println!("{}", ans);
 }
