a1e3da6438d152b82e393d81f9c73e87ed8f5a6a
nobushi95 <nobushi1121@gmail.com>
2023-10-22 16:33:16 +0900

    add abc032/B

--- contest_rust/abc032/src/bin/b.rs
+++ contest_rust/abc032/src/bin/b.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        k: usize,
+    };
+    let ans = s.windows(k).map(|x| x.iter().join("")).unique().count();
+    println!("{}", ans);
 }
