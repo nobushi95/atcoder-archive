2412bd80bd14b32177589728bc3da5b46c2d240d
nobushi95 <nobushi1121@gmail.com>
2023-10-23 23:30:11 +0900

    add abc032/A

--- contest_rust/abc032/src/bin/a.rs
+++ contest_rust/abc032/src/bin/a.rs
@@ -1,7 +1,15 @@
 use itertools::Itertools;
+use num_integer::lcm;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        n: i64,
+    };
+    let x = lcm(a, b);
+    let ans = if n % x == 0 { n } else { (n / x + 1) * x };
+    println!("{}", ans);
 }
