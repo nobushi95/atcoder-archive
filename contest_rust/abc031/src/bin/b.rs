1da725828790523155243df3ba599aefa827c172
nobushi95 <nobushi1121@gmail.com>
2023-10-24 23:51:41 +0900

    add abc031/B

--- contest_rust/abc031/src/bin/b.rs
+++ contest_rust/abc031/src/bin/b.rs
@@ -3,5 +3,23 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        l: i64,
+        h: i64,
+        n: usize,
+        a: [i64; n],
+    };
+    let ans = a
+        .iter()
+        .map(|&x| {
+            if x >= l && x <= h {
+                0
+            } else if x < l {
+                l - x
+            } else {
+                -1
+            }
+        })
+        .collect::<Vec<i64>>();
+    ans.iter().for_each(|x| println!("{}", x));
 }
