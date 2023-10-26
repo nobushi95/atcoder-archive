7149925768a79568dd048967a340a69bd9afa7ae
nobushi95 <nobushi1121@gmail.com>
2023-10-26 23:42:00 +0900

    add abc030/B

--- contest_rust/abc030/src/bin/b.rs
+++ contest_rust/abc030/src/bin/b.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        m: i64
+    };
+    let n = n as f64;
+    let m = m as f64;
+    let d = (n * 60. + m) * 5.5 % 360.;
+    let ans = d.min(360. - d);
+    println!("{:.4}", ans);
 }
