c72cfd162cd0d3623ddd7b6e7f14ee725ae6656e
nobushi95 <nobushi1121@gmail.com>
2023-10-27 22:01:31 +0900

    add abc030/A

--- contest_rust/abc030/src/bin/a.rs
+++ contest_rust/abc030/src/bin/a.rs
@@ -3,5 +3,18 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+        d: i64,
+    };
+    let takahashi = b * c;
+    let aoki = d * a;
+    let ans = match takahashi.cmp(&aoki) {
+        Ordering::Less => "AOKI",
+        Ordering::Equal => "DRAW",
+        Ordering::Greater => "TAKAHASHI",
+    };
+    println!("{}", ans);
 }
