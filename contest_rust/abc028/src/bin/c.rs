7b155fdf38b96eede4c374b378e2721a2cb72fff
nobushi95 <nobushi1121@gmail.com>
2023-10-31 00:09:38 +0900

    add abc028/C

--- contest_rust/abc028/src/bin/c.rs
+++ contest_rust/abc028/src/bin/c.rs
@@ -1,7 +1,15 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! { n: [i64; 5] };
+    let ans = n
+        .iter()
+        .combinations(3)
+        .map(|x| x.into_iter().sum::<i64>())
+        .sorted_by(|a, b| b.cmp(a))
+        .nth(2)
+        .unwrap();
+    println!("{}", ans);
 }
