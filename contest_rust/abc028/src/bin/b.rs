573c99f6c911f6ae2f2c2bd5e8c443c5c13eafe2
nobushi95 <nobushi1121@gmail.com>
2023-11-02 00:32:24 +0900

    add abc028/B

--- contest_rust/abc028/src/bin/b.rs
+++ contest_rust/abc028/src/bin/b.rs
@@ -1,7 +1,24 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashMap};
 
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    };
+    let mut tbl = HashMap::new();
+    s.iter().for_each(|&x| *tbl.entry(x).or_insert(0) += 1);
+
+    for c in vec!['A', 'B', 'C', 'D', 'E', 'F'] {
+        let ans = match tbl.get(&c) {
+            Some(v) => *v,
+            None => 0,
+        };
+        if c != 'F' {
+            print!("{} ", ans)
+        } else {
+            print!("{}", ans)
+        }
+    }
+    println!("");
 }
