7dd3487bd63fa8d452f85acbb181c32fa8ae14b7
nobushi95 <nobushi1121@gmail.com>
2023-11-04 15:43:48 +0900

    add abc028/A

--- contest_rust/abc028/src/bin/a.rs
+++ contest_rust/abc028/src/bin/a.rs
@@ -3,5 +3,15 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+    let ans = match n {
+        ..=59 => "Bad",
+        ..=89 => "Good",
+        ..=99 => "Great",
+        100 => "Perfect",
+        _ => unreachable!(""),
+    };
+    println!("{}", ans);
 }
