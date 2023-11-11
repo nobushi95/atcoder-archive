b5c55ee56e7bb0e02537d252fb773c5e66c7565d
nobushi95 <nobushi1121@gmail.com>
2023-11-11 17:46:56 +0900

    add abc024/B

--- contest_rust/abc024/src/bin/b.rs
+++ contest_rust/abc024/src/bin/b.rs
@@ -3,5 +3,22 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        t: i64,
+        a: [i64; n],
+    };
+    let ans = a
+        .windows(2)
+        .map(|x| {
+            let d = x[1] - x[0];
+            if d < t {
+                d
+            } else {
+                t
+            }
+        })
+        .sum::<i64>()
+        + t;
+    println!("{ans}");
 }
