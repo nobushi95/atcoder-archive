dab1987134839f3bcf3f1561e6d223faf2f7dd09
nobushi95 <nobushi1121@gmail.com>
2023-11-12 12:54:03 +0900

    add abc024/A

--- contest_rust/abc024/src/bin/a.rs
+++ contest_rust/abc024/src/bin/a.rs
@@ -3,5 +3,15 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+        k: i64,
+        s: i64,
+        t: i64,
+    };
+    let sum = a * s + t * b;
+    let ans = if s + t >= k { sum - c * (s + t) } else { sum };
+    println!("{ans}");
 }
