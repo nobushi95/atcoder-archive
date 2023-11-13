265d2e02c1dd7656538a1739c40593db03415650
nobushi95 <nobushi1121@gmail.com>
2023-11-13 23:34:31 +0900

    add abc023/B

--- contest_rust/abc023/src/bin/b.rs
+++ contest_rust/abc023/src/bin/b.rs
@@ -1,7 +1,35 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::VecDeque};
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        s: Chars,
+    };
+    let mut v = VecDeque::new();
+    v.push_back('b');
+    for i in 1..=(n / 2) {
+        match i % 3 {
+            1 => {
+                v.push_front('a');
+                v.push_back('c');
+            }
+            2 => {
+                v.push_front('c');
+                v.push_back('a');
+            }
+            0 => {
+                v.push_front('b');
+                v.push_back('b');
+            }
+            _ => unreachable!(),
+        }
+    }
+    let ans = if s.iter().join("") == v.iter().join("") {
+        n / 2
+    } else {
+        -1
+    };
+    println!("{ans}");
 }
