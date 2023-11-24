5d780be7d6c35f196dadac10b9ea987c91f68548
nobushi95 <nobushi1121@gmail.com>
2023-11-24 18:19:30 +0900

    add abc018/A

--- contest_rust/abc018/src/bin/a.rs
+++ contest_rust/abc018/src/bin/a.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+    };
+    let score = vec![a, b, c];
+    let order = score.iter().sorted().rev().collect_vec();
+    score
+        .iter()
+        .for_each(|&i| println!("{}", order.iter().position(|&&x| x == i).unwrap() + 1));
 }
