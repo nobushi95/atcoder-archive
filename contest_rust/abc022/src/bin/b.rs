2979d8d8471c7d9706cab1ed0e7a7cfd232cbdb5
nobushi95 <nobushi1121@gmail.com>
2023-11-27 14:48:43 +0900

    add abc022/B

--- contest_rust/abc022/src/bin/b.rs
+++ contest_rust/abc022/src/bin/b.rs
@@ -1,7 +1,18 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashMap};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+    };
+    let mut tbl = HashMap::new();
+    a.iter().for_each(|&x| *tbl.entry(x).or_insert(0) += 1);
+    let ans = tbl
+        .iter()
+        .filter(|(_, &x)| x > 1)
+        .map(|(_, &x)| x - 1)
+        .sum::<i64>();
+    println!("{ans}");
 }
