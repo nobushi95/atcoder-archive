44fad80d6f9d59f5f921868ba67f7c52c0e9ef93
nobushi95 <nobushi1121@gmail.com>
2023-11-29 00:23:22 +0900

    add abc017/A

--- contest_rust/abc017/src/bin/a.rs
+++ contest_rust/abc017/src/bin/a.rs
@@ -3,5 +3,9 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        se: [(i64, i64); 3],
+    };
+    let ans = se.iter().map(|(s, e)| s * e / 10).sum::<i64>();
+    println!("{ans}");
 }
