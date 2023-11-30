44e866cac0ef97ae8820fe0a7b184659a8ff5627
nobushi95 <nobushi1121@gmail.com>
2023-12-01 00:18:02 +0900

    add abc016/A

--- contest_rust/abc016/src/bin/a.rs
+++ contest_rust/abc016/src/bin/a.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        m: i64,
+        d: i64,
+    };
+    let ans = if m % d == 0 { "YES" } else { "NO" };
+    println!("{ans}");
 }
