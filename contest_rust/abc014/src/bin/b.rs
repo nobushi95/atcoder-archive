b631bef13ed018a3ece87cc9cc0fc6a71a9033e7
nobushi95 <nobushi1121@gmail.com>
2023-12-04 00:09:12 +0900

    add abc014/B

--- contest_rust/abc014/src/bin/b.rs
+++ contest_rust/abc014/src/bin/b.rs
@@ -3,5 +3,16 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        x: i64,
+        a: [i64; n],
+    };
+    let ans = format!("{:b}", x)
+        .chars()
+        .rev()
+        .enumerate()
+        .map(|(i, x)| if x == '1' { a[i] } else { 0 })
+        .sum::<i64>();
+    println!("{ans}");
 }
