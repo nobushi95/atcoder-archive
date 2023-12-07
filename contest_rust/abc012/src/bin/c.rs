ce565199853d3bebfb8e2e05222e9d2bcd2c6648
nobushi95 <nobushi1121@gmail.com>
2023-12-08 02:54:01 +0900

    add abc012/C

--- contest_rust/abc012/src/bin/c.rs
+++ contest_rust/abc012/src/bin/c.rs
@@ -3,5 +3,16 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+    let sum = 2025;
+    let product = sum - n;
+    for i in 1..=9 {
+        for j in 1..=9 {
+            if i * j == product {
+                println!("{i} x {j}");
+            }
+        }
+    }
 }
