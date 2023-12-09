a45e5ce0961ec41ad78c617db10b98a12c5d89e0
nobushi95 <nobushi1121@gmail.com>
2023-12-09 21:31:09 +0900

    add abc012/B

--- contest_rust/abc012/src/bin/b.rs
+++ contest_rust/abc012/src/bin/b.rs
@@ -3,5 +3,11 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+    let hour = n / 3600;
+    let minute = (n % 3600) / 60;
+    let seconds = n % 60;
+    println!("{hour:02}:{minute:02}:{seconds:02}");
 }
