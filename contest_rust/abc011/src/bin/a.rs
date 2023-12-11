d539ed22362100661831a08d700cfd28615b647b
nobushi95 <nobushi1121@gmail.com>
2023-12-12 00:16:13 +0900

    add abc011/A

--- contest_rust/abc011/src/bin/a.rs
+++ contest_rust/abc011/src/bin/a.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+    let next_month = n + 1;
+    let ans = if next_month == 12 {
+        next_month
+    } else {
+        next_month % 12
+    };
+    println!("{ans}");
 }
