688a4829032249e5d8e53c766ccf29e10f7e658b
nobushi95 <nobushi1121@gmail.com>
2023-12-11 23:56:21 +0900

    add abc011/B

--- contest_rust/abc011/src/bin/b.rs
+++ contest_rust/abc011/src/bin/b.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+         s: Chars
+    };
+    let ans = format!(
+        "{}{}",
+        s[0].to_uppercase(),
+        s.iter().skip(1).map(|x| x.to_lowercase()).join("")
+    );
+    println!("{ans}");
 }
