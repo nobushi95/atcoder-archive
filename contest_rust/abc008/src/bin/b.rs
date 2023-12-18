8bb89f445636b80b755bc34b96918bc5f5065d6c
nobushi95 <nobushi1121@gmail.com>
2023-12-18 23:13:41 +0900

    add abc008/B

--- contest_rust/abc008/src/bin/b.rs
+++ contest_rust/abc008/src/bin/b.rs
@@ -1,7 +1,14 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashMap};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    };
+    let mut tbl = HashMap::new();
+    s.iter().for_each(|x| *tbl.entry(x).or_insert(0) += 1);
+    let ans = tbl.iter().max_by(|x, y| x.1.cmp(y.1)).unwrap().0;
+    println!("{ans}");
 }
