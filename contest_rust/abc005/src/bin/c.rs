147c5c415988f19d0948570a086843091d598b99
nobushi95 <nobushi1121@gmail.com>
2023-12-24 18:32:19 +0900

    add abc005/C

--- contest_rust/abc005/src/bin/c.rs
+++ contest_rust/abc005/src/bin/c.rs
@@ -3,5 +3,38 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        t: i64,
+        n: usize,
+        a: [i64; n],
+        m: usize,
+        b: [i64; m],
+    };
+    if n < m {
+        println!("no");
+        return;
+    }
+
+    let mut tbl = vec![(0, true); n];
+    a.iter().enumerate().for_each(|(i, &x)| tbl[i].0 = x);
+
+    for bi in b {
+        let mut is_found = false;
+
+        for j in 0..n {
+            let d = bi - tbl[j].0;
+            if tbl[j].1 && d >= 0 && d <= t {
+                is_found = true;
+                tbl[j].1 = false;
+                break;
+            }
+        }
+
+        if !is_found {
+            println!("no");
+            return;
+        }
+    }
+
+    println!("yes");
 }
