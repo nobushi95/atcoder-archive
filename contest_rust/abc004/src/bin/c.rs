81c1a28e52309234dc33c480ef48462dfcf48edc
nobushi95 <nobushi1121@gmail.com>
2023-12-27 00:51:21 +0900

    add abc004/C

--- contest_rust/abc004/src/bin/c.rs
+++ contest_rust/abc004/src/bin/c.rs
@@ -3,5 +3,17 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    };
+    let n = n % 30;
+    let mut tbl = (1..=6).collect_vec();
+    for i in 0..n {
+        let l = i % 5;
+        let r = (i % 5) + 1;
+        tbl.swap(l, r);
+    }
+
+    let ans = tbl.iter().join("");
+    println!("{ans}");
 }
