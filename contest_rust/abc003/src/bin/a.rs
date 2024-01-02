ceb243228991d077a820e8b047b5d08ef15835ec
nobushi95 <nobushi1121@gmail.com>
2024-01-02 23:57:03 +0900

    add abc003/A

--- contest_rust/abc003/src/bin/a.rs
+++ contest_rust/abc003/src/bin/a.rs
@@ -3,5 +3,9 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    };
+    let ans = (1..=n).map(|x| (x * 10000) as f64).sum::<f64>() / n as f64;
+    println!("{:.6}", ans);
 }
