9606765ae0c6e1267db684cf11655cabc99baaae
nobushi95 <nobushi1121@gmail.com>
2024-01-03 16:07:44 +0900

    add abc002/B

--- contest_rust/abc002/src/bin/b.rs
+++ contest_rust/abc002/src/bin/b.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        w: Chars
+    };
+    let except_tbl = String::from("aiueo").chars().collect_vec();
+    let ans = w.iter().filter(|x| !except_tbl.contains(x)).join("");
+    println!("{ans}");
 }
