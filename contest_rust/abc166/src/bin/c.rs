d8a4178ee6b80b8172fb5c794ec4b57fc33610e5
nobushi95 <nobushi1121@gmail.com>
2024-01-07 19:19:48 +0900

    add abc166/C

--- contest_rust/abc166/src/bin/c.rs
+++ contest_rust/abc166/src/bin/c.rs
@@ -1,7 +1,31 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        h: [usize; n],
+        ab: [(usize, usize); m],
+    }
+    let mut tbl = vec![HashSet::new(); n];
+    ab.iter().for_each(|(a, b)| {
+        tbl[a - 1].insert(b - 1);
+        tbl[b - 1].insert(a - 1);
+    });
+
+    let mut ans = 0;
+    for i in 0..n {
+        let mut is_ok = true;
+        for another in &tbl[i] {
+            if h[i] <= h[*another] {
+                is_ok = false;
+            }
+        }
+        if is_ok {
+            ans += 1;
+        }
+    }
+
+    println!("{ans}");
 }
