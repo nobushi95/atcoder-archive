5b0b395cabf616b5322bec826d789a3493dfde63
nobushi95 <nobushi1121@gmail.com>
2024-01-08 16:45:21 +0900

    add abc021/B

--- contest_rust/abc021/src/bin/b.rs
+++ contest_rust/abc021/src/bin/b.rs
@@ -1,7 +1,23 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: usize, b: usize,
+        k: usize,
+        p: [usize; k],
+    };
+    let mut tbl = HashSet::new();
+    tbl.insert(a);
+    tbl.insert(b);
+    tbl.extend(&p);
+
+    let ans = if tbl.len() == p.len() + 2 {
+        "YES"
+    } else {
+        "NO"
+    };
+    println!("{ans}");
 }
