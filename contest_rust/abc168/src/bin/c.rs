c5392d342211cb711ef0aca94e888eceeab8817c
nobushi95 <nobushi1121@gmail.com>
2024-01-09 22:59:47 +0900

    add abc168/C

--- contest_rust/abc168/src/bin/c.rs
+++ contest_rust/abc168/src/bin/c.rs
@@ -1,7 +1,12 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::cmp::*;
+use std::f64::consts::PI;
 
 fn main() {
-    todo!();
+    input! { a: f64, b: f64, h: f64, m: f64 };
+    let rad = PI * 2.0 * (h / 12.0 + (m / 60.0) / 12.0 - (m / 60.0));
+    let rsq = (a * a + b * b) - (2.0 * a * b) * rad.cos();
+    let ans = rsq.sqrt();
+    println!("{:.10}", ans);
 }
