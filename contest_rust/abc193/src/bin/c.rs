b4ce5fafa4c2ec757dae783eaf136a4a1b22c7b1
nobushi95 <nobushi1121@gmail.com>
2024-01-13 03:27:25 +0900

    add abc193/C

--- contest_rust/abc193/src/bin/c.rs
+++ contest_rust/abc193/src/bin/c.rs
@@ -1,7 +1,17 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! { n: usize };
+    let mut s = HashSet::new();
+    for a in (2_usize..).take_while(|&i| i.pow(2) <= n) {
+        let mut x = a.pow(2);
+        while x <= n {
+            s.insert(x);
+            x *= a;
+        }
+    }
+    let ans = n - s.len();
+    println!("{ans}");
 }
