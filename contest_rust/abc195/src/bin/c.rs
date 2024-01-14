840d1c67043a87c3d73e79dda7ac5e70dcbad697
nobushi95 <nobushi1121@gmail.com>
2024-01-15 00:23:57 +0900

    add abc195/C

--- contest_rust/abc195/src/bin/c.rs
+++ contest_rust/abc195/src/bin/c.rs
@@ -3,5 +3,9 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! { n: usize, };
+    let ans = (1..=5)
+        .map(|x| n.saturating_sub(1000_usize.pow(x) - 1_usize))
+        .sum::<usize>();
+    println!("{ans}");
 }
