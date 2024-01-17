86b02580002a3a056c47c0522df0bbdd5d2673ec
nobushi95 <nobushi1121@gmail.com>
2024-01-18 00:54:29 +0900

    add abc270/B

--- contest_rust/abc270/src/bin/b.rs
+++ contest_rust/abc270/src/bin/b.rs
@@ -3,5 +3,24 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        mut x: i64,
+        mut y: i64,
+        mut z: i64,
+    };
+
+    if y < 0 {
+        (x, y, z) = (-x, -y, -z);
+    };
+
+    let ans = if x < y {
+        x.abs()
+    } else {
+        if z > y {
+            -1
+        } else {
+            z.abs() + (x - z).abs()
+        }
+    };
+    println!("{ans}");
 }
