e724c540f35d2ef6e64b2fbf82b37451288433ab
nobushi95 <nobushi1121@gmail.com>
2024-01-21 13:32:47 +0900

    add abc272/B

--- contest_rust/abc272/src/bin/b.rs
+++ contest_rust/abc272/src/bin/b.rs
@@ -1,7 +1,31 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, vec};
 
 fn main() {
-    todo!();
+    input! { n:usize, m:usize };
+    let mut tbl = vec![vec![false; n]; n];
+    for _ in 0..m {
+        input! {
+            k: usize,
+            x: [Usize1; k],
+        };
+        for i in 0..k {
+            for j in i + 1..k {
+                tbl[x[i]][x[j]] = true;
+                tbl[x[j]][x[i]] = true;
+            }
+        }
+    }
+
+    for i in 0..n {
+        for j in i + 1..n {
+            if !tbl[i][j] {
+                println!("No");
+                return;
+            }
+        }
+    }
+
+    println!("Yes");
 }
