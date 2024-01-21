0eaa5ce5615865a11cb273bf8f84d8e8245f00fe
nobushi95 <nobushi1121@gmail.com>
2024-01-22 00:50:49 +0900

    add abc272/A

--- contest_rust/abc272/src/bin/a.rs
+++ contest_rust/abc272/src/bin/a.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+    let ans = a.iter().sum::<usize>();
+    println!("{ans}");
 }
