4c725715c3ce87ddb790d7d224f64da228cbebbb
nobushi95 <nobushi1121@gmail.com>
2024-01-25 00:30:32 +0900

    add abc276/C

--- contest_rust/abc276/src/bin/c.rs
+++ contest_rust/abc276/src/bin/c.rs
@@ -1,7 +1,14 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::cmp::*;
+use superslice::Ext;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut p: [usize; n],
+    };
+    p.prev_permutation();
+    let ans = p.iter().join(" ");
+    println!("{ans}");
 }
