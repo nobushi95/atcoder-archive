81eda6bad054560baa745ea6d7246f67860d6ddf
nobushi95 <nobushi1121@gmail.com>
2024-01-27 23:45:58 +0900

    add abc276/A

--- contest_rust/abc276/src/bin/a.rs
+++ contest_rust/abc276/src/bin/a.rs
@@ -1,7 +1,14 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, fmt::format};
 
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    };
+    let ans = match s.iter().rposition(|&x| x == 'a') {
+        Some(x) => format!("{}", x + 1),
+        None => format!("-1"),
+    };
+    println!("{ans}");
 }
