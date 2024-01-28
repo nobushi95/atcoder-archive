7f32c49679caac594688c513beb9e90ae31f3bff
nobushi95 <nobushi1121@gmail.com>
2024-01-28 16:13:19 +0900

    add abc277/B

--- contest_rust/abc277/src/bin/b.rs
+++ contest_rust/abc277/src/bin/b.rs
@@ -3,5 +3,18 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [Chars; n],
+    };
+    let first_tbl = "HDCS".chars().collect_vec();
+    let second_tbl = "A23456789TJQK".chars().collect_vec();
+    let is_correct_string = |x: &Vec<char>| first_tbl.contains(&x[0]) && second_tbl.contains(&x[1]);
+    let ans =
+        if s.iter().all(|x| is_correct_string(x)) && s.len() == s.iter().sorted().dedup().count() {
+            "Yes"
+        } else {
+            "No"
+        };
+    println!("{ans}");
 }
