b9cfec8f2c7dbc8d65d84ceac980f9b006ef0751
nobushi95 <nobushi1121@gmail.com>
2024-01-30 00:48:38 +0900

    add abc278/C

--- contest_rust/abc278/src/bin/c.rs
+++ contest_rust/abc278/src/bin/c.rs
@@ -1,7 +1,30 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! { n: usize, q: usize };
+
+    let mut tbl = HashSet::<(usize, usize)>::new();
+
+    for _ in 0..q {
+        input! { t:usize, a: usize, b:usize };
+
+        match t {
+            1 => {
+                tbl.insert((a, b));
+            }
+            2 => {
+                tbl.remove(&(a, b));
+            }
+            _ => {
+                let ans = if tbl.contains(&(a, b)) && tbl.contains(&(b, a)) {
+                    "Yes"
+                } else {
+                    "No"
+                };
+                println!("{ans}");
+            }
+        }
+    }
 }
