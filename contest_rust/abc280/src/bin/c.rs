18c96214637f296b32a27d0a732067f2f07d7ee0
nobushi95 <nobushi1121@gmail.com>
2024-02-05 00:36:10 +0900

    add abc280/C

--- contest_rust/abc280/src/bin/c.rs
+++ contest_rust/abc280/src/bin/c.rs
@@ -3,5 +3,15 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    };
+    for (i, (si, ti)) in s.iter().zip(t.iter()).enumerate() {
+        if si != ti {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+    println!("{}", t.len());
 }
