a6735d988ac96caa3e6e806615e17c8dede5585e
nobushi95 <nobushi1121@gmail.com>
2024-02-07 00:41:47 +0900

    add abc280/A

--- contest_rust/abc280/src/bin/a.rs
+++ contest_rust/abc280/src/bin/a.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        h: usize, w: usize,
+        s: [Chars; h],
+    };
+    let ans = s
+        .iter()
+        .map(|x| x.iter().filter(|&&x| x == '#').count())
+        .sum::<usize>();
+    println!("{ans}");
 }
