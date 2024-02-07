f4fdf5f60bf5772a3939f3c9f2da7ea4e5ddd095
nobushi95 <nobushi1121@gmail.com>
2024-02-07 23:43:23 +0900

    add abc281/C

--- contest_rust/abc281/src/bin/c.rs
+++ contest_rust/abc281/src/bin/c.rs
@@ -3,5 +3,17 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, t:i64,
+        a: [i64; n],
+    };
+    let mut t = t % a.iter().sum::<i64>();
+    for (i, ai) in a.iter().enumerate() {
+        let remain = t;
+        t -= ai;
+        if t < 0 {
+            println!("{} {remain}", i + 1);
+            return;
+        }
+    }
 }
