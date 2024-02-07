bd915e3dc42ca0307616296d65baba26dcd1c664
nobushi95 <nobushi1121@gmail.com>
2024-02-08 00:14:48 +0900

    add abc281/B

--- contest_rust/abc281/src/bin/b.rs
+++ contest_rust/abc281/src/bin/b.rs
@@ -3,5 +3,28 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! { s: Chars };
+    if s.len() != 8 {
+        println!("No");
+        return;
+    }
+
+    let s_number = match s[1..s.len() - 1].iter().join("").parse::<i64>() {
+        Ok(x) => x,
+        Err(_) => {
+            println!("No");
+            return;
+        }
+    };
+
+    let ans = if s.first().unwrap().is_alphabetic()
+        && s.last().unwrap().is_alphabetic()
+        && s_number >= 100000
+        && s_number <= 999999
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{ans}");
 }
