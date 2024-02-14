94a7c830a161a9f874a6aebdd996706d2e9dee5b
nobushi95 <nobushi1121@gmail.com>
2024-02-15 01:05:48 +0900

    add abc283/B

--- contest_rust/abc283/src/bin/b.rs
+++ contest_rust/abc283/src/bin/b.rs
@@ -3,5 +3,23 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [usize; n],
+        q: usize,
+    };
+    for _ in 0..q {
+        input! { cmd: usize };
+        match cmd {
+            1 => {
+                input! { k: Usize1, x: usize };
+                a[k] = x;
+            }
+            2 => {
+                input! { k: Usize1 };
+                println!("{}", a[k]);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
