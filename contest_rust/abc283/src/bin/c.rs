a85f1dd0d1929df98aa75dba134ecf0496302345
nobushi95 <nobushi1121@gmail.com>
2024-02-15 01:04:51 +0900

    add abc283/C

--- contest_rust/abc283/src/bin/c.rs
+++ contest_rust/abc283/src/bin/c.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! { mut s: Chars };
+    let mut ans = s.len();
+    for i in 0..s.len() - 1 {
+        if s[i] == '0' && s[i + 1] == '0' {
+            s[i + 1] = 'a';
+            ans -= 1;
+        }
+    }
+
+    println!("{ans}");
 }
