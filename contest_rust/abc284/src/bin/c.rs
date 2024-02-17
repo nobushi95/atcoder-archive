86befdd3ac4f2be3be2d2c90e036897d9fb96ea8
nobushi95 <nobushi1121@gmail.com>
2024-02-17 22:31:47 +0900

    add abc284/C

--- contest_rust/abc284/src/bin/c.rs
+++ contest_rust/abc284/src/bin/c.rs
@@ -1,7 +1,18 @@
+use ac_library::Dsu;
 use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        uv: [(Usize1, Usize1); m],
+    };
+    let mut g = Dsu::new(n);
+    uv.iter().for_each(|(u, v)| {
+        g.merge(*u, *v);
+    });
+    let ans = g.groups().len();
+
+    println!("{ans}");
 }
