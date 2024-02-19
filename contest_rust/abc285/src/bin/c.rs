e7de03b24d5ff5f78f5a7a973ef25a7c641e063a
nobushi95 <nobushi1121@gmail.com>
2024-02-20 00:01:20 +0900

    add abc285/C

--- contest_rust/abc285/src/bin/c.rs
+++ contest_rust/abc285/src/bin/c.rs
@@ -1,7 +1,16 @@
 use itertools::Itertools;
+use num_traits::Pow;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! { s: Chars };
+    let ans = s
+        .iter()
+        .rev()
+        .map(|&x| x as u8 - 'A' as u8 + 1)
+        .enumerate()
+        .map(|(i, x)| x as i64 * 26_i64.pow(i as u32))
+        .sum::<i64>();
+    println!("{ans}");
 }
