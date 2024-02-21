465b2441decb735bd9802e075f02503bc7ef8d9d
nobushi95 <nobushi1121@gmail.com>
2024-02-22 01:40:18 +0900

    add abc285/A

--- contest_rust/abc285/src/bin/a.rs
+++ contest_rust/abc285/src/bin/a.rs
@@ -3,5 +3,11 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! { a: usize, b: usize };
+    let ans = if a * 2 == b || a * 2 + 1 == b {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{ans}");
 }
