310f62883581d963e91f258bafe69f0b86268655
nobushi95 <nobushi1121@gmail.com>
2024-02-25 00:17:29 +0900

    add abc287/B

--- contest_rust/abc287/src/bin/b.rs
+++ contest_rust/abc287/src/bin/b.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        s: [Chars; n],
+        t: [Chars; m],
+    };
+    let ans = s
+        .iter()
+        .filter(|x| t.iter().any(|y| x.ends_with(y)))
+        .count();
+    println!("{ans}");
 }
