1becd1f93f5bfa6cc4bf2836c265537eda6bffa5
nobushi95 <nobushi1121@gmail.com>
2024-02-26 00:47:35 +0900

    add abc287/A

--- contest_rust/abc287/src/bin/a.rs
+++ contest_rust/abc287/src/bin/a.rs
@@ -3,5 +3,16 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    };
+    let ans = if s.iter().filter(|&x| x == "For").count()
+        > s.iter().filter(|&x| x == "Against").count()
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{ans}");
 }
