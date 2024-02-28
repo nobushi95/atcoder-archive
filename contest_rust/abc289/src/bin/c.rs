ab8a10b772a365d647e5bf96e568b58f9389f6d6
nobushi95 <nobushi1121@gmail.com>
2024-02-29 00:13:31 +0900

    add abc289/C

--- contest_rust/abc289/src/bin/c.rs
+++ contest_rust/abc289/src/bin/c.rs
@@ -1,7 +1,34 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+    }
+
+    let mut aa = Vec::new();
+    for _ in 0..m {
+        input! {
+            c: usize,
+            a: [usize; c],
+        };
+        aa.push(a);
+    }
+
+    let mut ans = 0;
+    for b in 0..1 << m {
+        let mut s = HashSet::new();
+        for i in 0..m {
+            if (b >> i) & 1 != 0 {
+                aa[i].iter().for_each(|&x| {
+                    s.insert(x);
+                });
+            }
+        }
+        if s.len() == n {
+            ans += 1;
+        }
+    }
+    println!("{ans}");
 }
