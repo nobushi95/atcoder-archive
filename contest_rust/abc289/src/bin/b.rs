8e12719004420bd42535ee08887d9f3487241a3c
nobushi95 <nobushi1121@gmail.com>
2024-03-01 00:07:01 +0900

    add abc289/B

--- contest_rust/abc289/src/bin/b.rs
+++ contest_rust/abc289/src/bin/b.rs
@@ -3,5 +3,24 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        a: [usize; m],
+    };
+    let mut v = Vec::new();
+    let mut stack = Vec::new();
+
+    for i in 1..n + 1 {
+        if a.contains(&i) {
+            stack.push(i);
+        } else {
+            v.push(i);
+            while !stack.is_empty() {
+                v.push(stack.pop().unwrap());
+            }
+        }
+    }
+
+    let ans = v.iter().join(" ");
+    println!("{ans}");
 }
