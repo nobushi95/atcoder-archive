f8e826ddc5fb017c82cc9a8ab749c72c9bb40b57
nobushi95 <nobushi1121@gmail.com>
2024-03-03 15:00:44 +0900

    add abc290/C

--- contest_rust/abc290/src/bin/c.rs
+++ contest_rust/abc290/src/bin/c.rs
@@ -1,7 +1,18 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, k:usize,
+        mut a: [usize; n],
+    };
+    let a = a.iter().collect::<HashSet<_>>();
+
+    let ans = match (0..k).filter(|x| !a.contains(x)).next() {
+        Some(x) => x,
+        None => k,
+    };
+
+    println!("{ans}");
 }
