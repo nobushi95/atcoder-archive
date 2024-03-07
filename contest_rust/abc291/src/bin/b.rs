3a23cf70d75e85b78476f76c3207f49d621c182a
nobushi95 <nobushi1121@gmail.com>
2024-03-08 00:25:02 +0900

    add abc291/B

--- contest_rust/abc291/src/bin/b.rs
+++ contest_rust/abc291/src/bin/b.rs
@@ -3,5 +3,11 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut x: [usize; 5 * n],
+    };
+    x.sort();
+    let ans = x[n..(4 * n)].iter().sum::<usize>() as f64 / (3 * n) as f64;
+    println!("{:.6}", ans);
 }
