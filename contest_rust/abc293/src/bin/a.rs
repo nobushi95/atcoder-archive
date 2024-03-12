3e6d881a9e9710d659916a0c274ea5735bdaaf8e
nobushi95 <nobushi1121@gmail.com>
2024-03-13 00:24:40 +0900

    add abc293/A

--- contest_rust/abc293/src/bin/a.rs
+++ contest_rust/abc293/src/bin/a.rs
@@ -3,5 +3,12 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        mut s: Chars,
+    };
+    for i in (0..s.len() - 1).step_by(2) {
+        s.swap(i, i + 1);
+    }
+    let ans = s.iter().join("");
+    println!("{ans}");
 }
