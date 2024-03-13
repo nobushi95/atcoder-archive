4236db286d11a1cb7127b05e173abc4dfbb33f45
nobushi95 <nobushi1121@gmail.com>
2024-03-14 00:07:29 +0900

    add abc294/B

--- contest_rust/abc294/src/bin/b.rs
+++ contest_rust/abc294/src/bin/b.rs
@@ -3,5 +3,17 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        h: usize, w: usize,
+        a: [[u8; w]; h],
+    };
+    let ans = a
+        .iter()
+        .map(|x| {
+            x.iter()
+                .map(|&x| if x == 0 { '.' } else { (b'A' + x - 1) as char })
+                .join("")
+        })
+        .join("\n");
+    println!("{ans}");
 }
