50d85d38a1f4cc5a5e67a24415c0bed582d3171a
nobushi95 <nobushi1121@gmail.com>
2024-03-17 17:07:17 +0900

    add abc294/C

--- contest_rust/abc294/src/bin/c.rs
+++ contest_rust/abc294/src/bin/c.rs
@@ -3,5 +3,27 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        a: [usize; n],
+        b: [usize; m],
+    };
+    let a = a.iter().map(|x| (x, true)).collect_vec();
+    let b = b.iter().map(|x| (x, false)).collect_vec();
+    let mut c = vec![a, b].concat();
+    c.sort_by(|a, b| a.0.cmp(&b.0));
+    let ans_a = c
+        .iter()
+        .enumerate()
+        .filter(|(_, x)| x.1)
+        .map(|(i, _)| i + 1)
+        .join(" ");
+    let ans_b = c
+        .iter()
+        .enumerate()
+        .filter(|(_, x)| !x.1)
+        .map(|(i, _)| i + 1)
+        .join(" ");
+    println!("{ans_a}");
+    println!("{ans_b}");
 }
