dd2106193a16c146e03c02b9ecfe9e98c3a4013f
nobushi95 <nobushi1121@gmail.com>
2024-03-21 00:31:30 +0900

    mod abc296/B

--- contest_rust/abc296/src/bin/b.rs
+++ contest_rust/abc296/src/bin/b.rs
@@ -3,5 +3,15 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: [Chars; 8],
+    };
+    let tbl = "abcdefgh".chars().collect_vec();
+    for i in 0..8 {
+        for j in 0..8 {
+            if s[i][j] == '*' {
+                println!("{}{}", tbl[j], 8 - i);
+            }
+        }
+    }
 }
