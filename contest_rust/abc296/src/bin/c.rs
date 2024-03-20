c9c2198936b75f7c6c5f24da4cf6d2068c89e097
nobushi95 <nobushi1121@gmail.com>
2024-03-20 17:18:16 +0900

    mod abc296/C

--- contest_rust/abc296/src/bin/c.rs
+++ contest_rust/abc296/src/bin/c.rs
@@ -1,7 +1,18 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, x: i64,
+        a: [i64; n],
+    };
+    let a = a.iter().collect::<HashSet<_>>();
+    for ai in &a {
+        if a.contains(&(*ai + x)) || a.contains(&(*ai - x)) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
