a755878f60ae3a918c5e89779272dda2df960bb2
nobushi95 <nobushi1121@gmail.com>
2024-03-22 00:54:59 +0900

    mod abc296/A

--- contest_rust/abc296/src/bin/a.rs
+++ contest_rust/abc296/src/bin/a.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+    };
+    let ans = if s.windows(2).all(|x| x[0] != x[1]) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{ans}");
 }
