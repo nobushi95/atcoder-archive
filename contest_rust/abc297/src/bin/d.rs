648660ee9a786e98113f6c925de8a7ba43826879
nobushi95 <nobushi1121@gmail.com>
2024-03-23 01:41:17 +0900

    mod abc297/D

--- contest_rust/abc297/src/bin/d.rs
+++ contest_rust/abc297/src/bin/d.rs
@@ -1,7 +1,20 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, mem::swap};
 
 fn main() {
-    todo!();
+    input! {
+        mut a: i64, mut b: i64,
+    };
+    let mut ans = 0_i64;
+    if a < b {
+        swap(&mut a, &mut b);
+    }
+    while b > 0 {
+        ans += a / b;
+        a %= b;
+        swap(&mut a, &mut b);
+    }
+    ans -= 1;
+    println!("{ans}");
 }
