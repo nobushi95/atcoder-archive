d2c5cffda1ab68bb3312ab4c1140170ff997fb32
nobushi95 <nobushi1121@gmail.com>
2024-03-24 15:23:55 +0900

    mod abc297/C

--- contest_rust/abc297/src/bin/c.rs
+++ contest_rust/abc297/src/bin/c.rs
@@ -3,5 +3,18 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        h: usize, w: usize,
+        mut s: [Chars; h],
+    };
+    for i in 0..h {
+        for j in 0..w - 1 {
+            if s[i][j] == 'T' && s[i][j + 1] == 'T' {
+                s[i][j] = 'P';
+                s[i][j + 1] = 'C';
+            }
+        }
+    }
+    let ans = s.iter().map(|x| x.iter().join("")).join("\n");
+    println!("{ans}");
 }
