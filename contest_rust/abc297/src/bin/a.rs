8ca6574067f52daf2f2042c55bdd7871761d03b1
nobushi95 <nobushi1121@gmail.com>
2024-03-26 01:45:12 +0900

    mod abc297/A

--- contest_rust/abc297/src/bin/a.rs
+++ contest_rust/abc297/src/bin/a.rs
@@ -3,5 +3,15 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, d: usize,
+        t: [usize; n],
+    };
+    for i in 0..n - 1 {
+        if t[i + 1] - t[i] <= d {
+            println!("{}", t[i + 1]);
+            return;
+        }
+    }
+    println!("-1");
 }
