9f24f261921b5eeee7eae5ad906740baa39b6572
nobushi95 <nobushi1121@gmail.com>
2024-03-27 00:42:28 +0900

    add abc298/B

--- contest_rust/abc298/src/bin/b.rs
+++ contest_rust/abc298/src/bin/b.rs
@@ -2,6 +2,46 @@ use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::cmp::*;
 
+fn rotate(a: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let n = a.len();
+    let mut ret = vec![vec![0; n]; n];
+
+    for i in 0..n {
+        for j in 0..n {
+            ret[j][n - 1 - i] = a[i][j];
+        }
+    }
+
+    ret
+}
+
+fn is_valid(a: &Vec<Vec<usize>>, b: &Vec<Vec<usize>>) -> bool {
+    let n = a.len();
+    for i in 0..n {
+        for j in 0..n {
+            if a[i][j] == 1 && b[i][j] == 0 {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [[usize; n]; n],
+        b: [[usize; n]; n],
+    };
+
+    for _ in 0..4 {
+        if is_valid(&a, &b) {
+            println!("Yes");
+            return;
+        }
+        a = rotate(&a);
+    }
+
+    println!("No");
 }
