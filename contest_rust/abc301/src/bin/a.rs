caf8c395345fe24092d827e3dc1eba4b75fb9a99
nobushi95 <nobushi1121@gmail.com>
2024-04-04 01:00:03 +0900

    add abc301/A

--- contest_rust/abc301/src/bin/a.rs
+++ contest_rust/abc301/src/bin/a.rs
@@ -3,5 +3,27 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+    };
+    let t = s.iter().filter(|&x| *x == 'T').count();
+    let a = s.iter().filter(|&x| *x == 'A').count();
+
+    let ans = if t > a {
+        "T"
+    } else if t < a {
+        "A"
+    } else {
+        let t_pos = s.iter().positions(|&x| x == 'T');
+        let a_pos = s.iter().positions(|&x| x == 'A');
+        let t_last = t_pos.last().unwrap();
+        let a_last = a_pos.last().unwrap();
+        if t_last < a_last {
+            "T"
+        } else {
+            "A"
+        }
+    };
+    println!("{ans}");
 }
