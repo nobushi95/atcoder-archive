c29a298b3fd0412f3417f1125368d8929bbd0f0c
nobushi95 <nobushi1121@gmail.com>
2024-04-07 01:49:44 +0900

    add abc303/B

--- contest_rust/abc303/src/bin/b.rs
+++ contest_rust/abc303/src/bin/b.rs
@@ -3,5 +3,24 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        a: [[usize; n]; m],
+    };
+    let mut tbl = vec![vec![false; n]; n];
+    a.iter().for_each(|x| {
+        x.windows(2).for_each(|y| {
+            tbl[y[0] - 1][y[1] - 1] = true;
+            tbl[y[1] - 1][y[0] - 1] = true;
+        });
+    });
+    let mut ans = 0;
+    for i in 0..n {
+        for j in (i + 1)..n {
+            if !tbl[i][j] {
+                ans += 1;
+            }
+        }
+    }
+    println!("{ans}");
 }
