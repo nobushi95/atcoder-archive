d911fa8fc51bee91891591d8375a0da653220b8d
nobushi95 <nobushi1121@gmail.com>
2024-04-08 00:33:15 +0900

    add abc303/C

--- contest_rust/abc303/src/bin/c.rs
+++ contest_rust/abc303/src/bin/c.rs
@@ -1,7 +1,33 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize, mut h: i64, k: i64,
+        s: Chars,
+        xy: [(i64, i64); m],
+    };
+    let mut heal_point = xy.iter().collect::<HashSet<_>>();
+    let mut pos = (0_i64, 0_i64);
+    for c in s {
+        match c {
+            'R' => pos.0 += 1,
+            'L' => pos.0 -= 1,
+            'U' => pos.1 += 1,
+            'D' => pos.1 -= 1,
+            _ => unreachable!(),
+        };
+        h -= 1;
+        if h < 0 {
+            println!("No");
+            return;
+        }
+        if heal_point.contains(&pos) && h < k {
+            heal_point.remove(&pos);
+            h = k;
+        }
+    }
+
+    println!("Yes");
 }
