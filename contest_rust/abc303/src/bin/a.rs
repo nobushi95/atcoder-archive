146969c70fb9bf85ee30f83f0f0c58106f762c65
nobushi95 <nobushi1121@gmail.com>
2024-04-09 00:49:22 +0900

    add abc303/A

--- contest_rust/abc303/src/bin/a.rs
+++ contest_rust/abc303/src/bin/a.rs
@@ -3,5 +3,21 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+        t: Chars,
+    };
+    let ans = if s.iter().zip(t.iter()).all(|(&x, &y)| {
+        x == y
+            || (x == '1' && y == 'l')
+            || (x == 'l' && y == '1')
+            || (x == '0' && y == 'o')
+            || (x == 'o' || y == '0')
+    }) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{ans}");
 }
