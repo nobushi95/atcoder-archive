526206d5d811e2c1e54345db292c9e22aa6cd4f2
nobushi95 <nobushi1121@gmail.com>
2024-04-10 00:30:16 +0900

    add abc304/B

--- contest_rust/abc304/src/bin/b.rs
+++ contest_rust/abc304/src/bin/b.rs
@@ -3,5 +3,18 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    };
+    let ans = match n {
+        ..=999 => n,
+        ..=9999 => n - n % 10,
+        ..=99999 => n - n % 100,
+        ..=999999 => n - n % 1000,
+        ..=9999999 => n - n % 10000,
+        ..=99999999 => n - n % 100000,
+        ..=999999999 => n - n % 1000000,
+        _ => unreachable!(),
+    };
+    println!("{ans}");
 }
