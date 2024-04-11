544feed4024dc1b97747f75b304ee295402179b4
nobushi95 <nobushi1121@gmail.com>
2024-04-12 00:25:47 +0900

    add abc304/A

--- contest_rust/abc304/src/bin/a.rs
+++ contest_rust/abc304/src/bin/a.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        sa: [(String, usize); n],
+    };
+    let min_age_pos = sa.iter().position_min_by(|x, y| x.1.cmp(&y.1)).unwrap();
+    for i in 0..n {
+        let idx = (min_age_pos + i) % n;
+        println!("{}", sa[idx].0);
+    }
 }
