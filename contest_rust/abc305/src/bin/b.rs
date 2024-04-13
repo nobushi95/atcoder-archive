b95f0c4b6d23ce8ec896f3cbeab2f675e44592a5
nobushi95 <nobushi1121@gmail.com>
2024-04-13 23:56:33 +0900

    add abc305/B

--- contest_rust/abc305/src/bin/b.rs
+++ contest_rust/abc305/src/bin/b.rs
@@ -3,5 +3,19 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        p: char, q: char,
+    };
+    let distance = [0, 3, 1, 4, 1, 5, 9];
+    let pos = distance
+        .iter()
+        .scan(0, |state, &x| {
+            *state += x;
+            Some(*state)
+        })
+        .collect_vec();
+    let right = (p.max(q) as u8 - b'A') as usize;
+    let left = (p.min(q) as u8 - b'A') as usize;
+    let ans = pos[right] - pos[left];
+    println!("{ans}");
 }
