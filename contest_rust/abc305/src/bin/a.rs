d74c6aeb7b355e173dc4f8447855d7ffa3bacff8
nobushi95 <nobushi1121@gmail.com>
2024-04-15 01:28:18 +0900

    add abc305/A

--- contest_rust/abc305/src/bin/a.rs
+++ contest_rust/abc305/src/bin/a.rs
@@ -3,5 +3,16 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    };
+    let rem = n % 5;
+    let ans = if rem == 0 {
+        n
+    } else if rem > 2 {
+        n + 5 - rem
+    } else {
+        n - rem
+    };
+    println!("{ans}");
 }
