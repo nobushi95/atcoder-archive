2dbba0f50ae91a0c21132cefbaad1c8dcc607e94
nobushi95 <nobushi1121@gmail.com>
2024-04-17 00:05:37 +0900

    add abc306/B

--- contest_rust/abc306/src/bin/b.rs
+++ contest_rust/abc306/src/bin/b.rs
@@ -3,5 +3,9 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: [char; 64],
+    };
+    let ans = u64::from_str_radix(a.iter().rev().join("").as_str(), 2).unwrap();
+    println!("{ans}");
 }
