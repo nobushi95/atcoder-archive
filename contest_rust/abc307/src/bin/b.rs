dc1dff752d98f53373d664670c9d42f8f10994ac
nobushi95 <nobushi1121@gmail.com>
2024-04-19 00:25:16 +0900

    add abc307/B

--- contest_rust/abc307/src/bin/b.rs
+++ contest_rust/abc307/src/bin/b.rs
@@ -3,5 +3,23 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    };
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+
+            let cur = format!("{}{}", s[i], s[j]);
+            let cur_rev = cur.chars().rev().join("");
+            if cur == cur_rev {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
