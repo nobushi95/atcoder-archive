8c7e79aa794b9355d01fc2be6533d2788fff8fdd
nobushi95 <nobushi1121@gmail.com>
2024-04-22 00:29:31 +0900

    add abc308/B

--- contest_rust/abc308/src/bin/b.rs
+++ contest_rust/abc308/src/bin/b.rs
@@ -1,7 +1,22 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashMap};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        c: [String; n],
+        d: [String; m],
+        p: [u64; m + 1],
+    };
+    let price_tbl = d
+        .iter()
+        .zip(p[1..].iter())
+        .map(|(key, &value)| (key, value))
+        .collect::<HashMap<_, _>>();
+    let ans = c
+        .iter()
+        .map(|x| price_tbl.get(x).unwrap_or(&&p[0]))
+        .sum::<u64>();
+    println!("{ans}");
 }
