c82932e4517e4df741c720814251cfbb90376f1e
nobushi95 <nobushi1121@gmail.com>
2024-04-23 00:55:30 +0900

    add abc308/A

--- contest_rust/abc308/src/bin/a.rs
+++ contest_rust/abc308/src/bin/a.rs
@@ -3,5 +3,15 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: [u64; 8],
+    };
+    let ans = if s.windows(2).all(|x| x[0] <= x[1])
+        && s.iter().all(|&x| x >= 100 && x <= 675 && x % 25 == 0)
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{ans}");
 }
