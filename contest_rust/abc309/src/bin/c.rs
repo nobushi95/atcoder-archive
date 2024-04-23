bffd7597e7f164d357fac5dcc78a3f8004bd4a32
nobushi95 <nobushi1121@gmail.com>
2024-04-24 00:02:25 +0900

    add abc309/C

--- contest_rust/abc309/src/bin/c.rs
+++ contest_rust/abc309/src/bin/c.rs
@@ -3,5 +3,23 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, k: u64,
+        mut ab: [(u64, u64); n],
+    };
+    ab.sort();
+    let mut sum_medicine = ab.iter().map(|x| x.1).sum::<u64>();
+    if sum_medicine <= k {
+        println!("1");
+        return;
+    }
+
+    for i in 0..n {
+        if sum_medicine <= k {
+            println!("{}", ab[i - 1].0 + 1);
+            return;
+        }
+        sum_medicine -= ab[i].1;
+    }
+    println!("{}", ab.last().unwrap().0 + 1);
 }
