d49a48301727820550c9b020e14ccb83d0dd5408
nobushi95 <nobushi1121@gmail.com>
2024-04-25 00:02:18 +0900

    add abc309/B

--- contest_rust/abc309/src/bin/b.rs
+++ contest_rust/abc309/src/bin/b.rs
@@ -1,7 +1,19 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, vec};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [Chars; n],
+    };
+    let mut b = a.clone();
+    for j in 1..n {
+        b[0][j] = a[0][j - 1];
+        b[j][n - 1] = a[j - 1][n - 1];
+        b[n - 1][n - 1 - j] = a[n - 1][n - j];
+        b[n - 1 - j][0] = a[n - j][0];
+    }
+    let ans = b.iter().map(|x| x.iter().join("")).join("\n");
+    println!("{ans}");
 }
