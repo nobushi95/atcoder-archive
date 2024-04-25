9e98925fa49974ae53ba0e6febb124a0d753cb3b
nobushi95 <nobushi1121@gmail.com>
2024-04-26 00:19:00 +0900

    add abc309/A

--- contest_rust/abc309/src/bin/a.rs
+++ contest_rust/abc309/src/bin/a.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        a: u64, b: u64,
+    };
+    let ans = if b - a == 1 && (a != 3 && a != 6 && a != 9) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{ans}");
 }
