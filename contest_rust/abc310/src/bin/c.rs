3a32a70cd1e033e50a621268df2f22c523a0f12b
nobushi95 <nobushi1121@gmail.com>
2024-04-27 23:39:29 +0900

    add abc310/C

--- contest_rust/abc310/src/bin/c.rs
+++ contest_rust/abc310/src/bin/c.rs
@@ -1,7 +1,19 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
-use std::cmp::*;
+use std::{cmp::*, collections::HashSet};
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    };
+    let mut tbl = HashSet::new();
+    for si in s.iter() {
+        if tbl.contains(si) || tbl.contains(&si.chars().rev().join("")) {
+            continue;
+        }
+        tbl.insert(si);
+    }
+    let ans = tbl.len();
+    println!("{ans}");
 }
