630a58ef147478b62068227fffb2da87e6386c15
nobushi95 <nobushi1121@gmail.com>
2024-04-29 00:15:11 +0900

    add abc310/A

--- contest_rust/abc310/src/bin/a.rs
+++ contest_rust/abc310/src/bin/a.rs
@@ -3,5 +3,10 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, p: u64, q: u64,
+        d: [u64; n],
+    };
+    let ans = p.min(q + d.iter().min().unwrap());
+    println!("{ans}");
 }
