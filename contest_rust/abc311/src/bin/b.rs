1213ef114b4b29cb1e3e630fa1decb54929b6e1e
nobushi95 <nobushi1121@gmail.com>
2024-05-02 23:40:03 +0900

    add abc311/B

--- contest_rust/abc311/src/bin/b.rs
+++ contest_rust/abc311/src/bin/b.rs
@@ -3,5 +3,23 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, d: usize,
+        s: [Chars; n],
+    };
+
+    let mut ans = 0;
+    for i in 0..d {
+        let mut cur = 0;
+        for j in i..d {
+            if s.iter().all(|x| x[j] == 'o') {
+                cur += 1;
+            } else {
+                break;
+            }
+        }
+        ans = ans.max(cur);
+    }
+
+    println!("{ans}");
 }
