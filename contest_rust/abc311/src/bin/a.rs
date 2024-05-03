48f5d33d82b1c711511fc1fc428f6db435e401b7
nobushi95 <nobushi1121@gmail.com>
2024-05-03 22:40:42 +0900

    add abc311/A

--- contest_rust/abc311/src/bin/a.rs
+++ contest_rust/abc311/src/bin/a.rs
@@ -3,5 +3,25 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+    };
+    let mut is_a_ok = false;
+    let mut is_b_ok = false;
+    let mut is_c_ok = false;
+    for i in 0..n {
+        if is_a_ok && is_b_ok && is_c_ok {
+            println!("{}", i);
+            return;
+        }
+
+        match s[i] {
+            'A' => is_a_ok = true,
+            'B' => is_b_ok = true,
+            'C' => is_c_ok = true,
+            _ => {}
+        };
+    }
+    println!("{n}");
 }
