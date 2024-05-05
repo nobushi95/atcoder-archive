6a0ca3eaa19fd99fe72eb33c50ce754cc6517bab
nobushi95 <nobushi1121@gmail.com>
2024-05-05 21:29:59 +0900

    add abc311/C

--- contest_rust/abc311/src/bin/c.rs
+++ contest_rust/abc311/src/bin/c.rs
@@ -3,5 +3,21 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    };
+    let mut v = 0;
+    let mut is_passed = vec![false; n];
+    let mut s = vec![];
+    while !is_passed[v] {
+        is_passed[v] = true;
+        s.push(v);
+        v = a[v] - 1;
+    }
+
+    let pos = s.iter().position(|&x| x == v).unwrap();
+    let ans = &s[pos..];
+    println!("{}", ans.len());
+    println!("{}", ans.iter().map(|x| x + 1).join(" "));
 }
