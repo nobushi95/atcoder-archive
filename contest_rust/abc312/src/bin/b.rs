d24574d186fa930cb9913defd18598dbac7b1ce4
nobushi95 <nobushi1121@gmail.com>
2024-05-07 00:10:45 +0900

    add abc312/B

--- contest_rust/abc312/src/bin/b.rs
+++ contest_rust/abc312/src/bin/b.rs
@@ -3,5 +3,69 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        s: [Chars; n],
+    };
+    let is_tak_code = |x: usize, y: usize| {
+        for i in x..x + 4 {
+            for j in y..y + 4 {
+                if i == x + 3 {
+                    if s[i][j] != '.' {
+                        return false;
+                    }
+                    continue;
+                }
+
+                if j == y + 3 {
+                    if s[i][j] != '.' {
+                        return false;
+                    }
+                    continue;
+                }
+
+                if s[i][j] != '#' {
+                    return false;
+                }
+            }
+        }
+
+        for i in x + 5..x + 9 {
+            for j in y + 5..y + 9 {
+                if i == x + 5 {
+                    if s[i][j] != '.' {
+                        return false;
+                    }
+                    continue;
+                }
+
+                if j == y + 5 {
+                    if s[i][j] != '.' {
+                        return false;
+                    }
+                    continue;
+                }
+
+                if s[i][j] != '#' {
+                    return false;
+                }
+            }
+        }
+
+        true
+    };
+
+    let mut ans = vec![];
+    for i in 0..n - 8 {
+        for j in 0..m - 8 {
+            if is_tak_code(i, j) {
+                ans.push((i + 1, j + 1));
+            }
+        }
+    }
+    if ans.len() == 0 {
+        return;
+    }
+    let ans = ans.iter().map(|(x, y)| format!("{x} {y}")).join("\n");
+    println!("{ans}");
 }
