b4fdbb50a01caed070d270548a9e9aa9ab0fd939
nobushi95 <nobushi1121@gmail.com>
2024-05-06 23:47:55 +0900

    add abc312/C

--- contest_rust/abc312/src/bin/c.rs
+++ contest_rust/abc312/src/bin/c.rs
@@ -3,5 +3,13 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        mut a: [u64; n],
+        b: [u64; m],
+    };
+    a.extend(b.iter().map(|x| x + 1));
+    a.sort();
+    let ans = a[m - 1];
+    println!("{ans}");
 }
