a5e07a4b49b95b9457c88c4560f4d83e451b4d29
nobushi95 <nobushi1121@gmail.com>
2024-05-08 00:06:29 +0900

    add abc312/A

--- contest_rust/abc312/src/bin/a.rs
+++ contest_rust/abc312/src/bin/a.rs
@@ -3,5 +3,14 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    };
+    let tbl = vec!["ACE", "BDF", "CEG", "DFA", "EGB", "FAC", "GBD"];
+    let ans = if tbl.contains(&s.as_str()) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{ans}");
 }
