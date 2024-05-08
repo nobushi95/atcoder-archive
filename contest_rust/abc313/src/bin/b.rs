59fd21676172740ae313cce65229382f94aa83ba
nobushi95 <nobushi1121@gmail.com>
2024-05-09 00:33:41 +0900

    add abc313/B

--- contest_rust/abc313/src/bin/b.rs
+++ contest_rust/abc313/src/bin/b.rs
@@ -7,5 +7,23 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        ab: [(Usize1, Usize1); m],
+    };
+    let mut tbl = vec![0; n];
+    ab.iter().for_each(|&(_, b)| {
+        tbl[b] += 1;
+    });
+    let candidates = tbl
+        .iter()
+        .enumerate()
+        .filter(|&(i, x)| *x == 0)
+        .collect_vec();
+    let ans = if candidates.len() == 1 {
+        (candidates.first().unwrap().0 + 1) as i64
+    } else {
+        -1
+    };
+    println!("{ans}");
 }
