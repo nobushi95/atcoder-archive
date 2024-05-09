f185674143669d7e360b821978832c650ba2ea81
nobushi95 <nobushi1121@gmail.com>
2024-05-10 00:16:26 +0900

    add abc313/A

--- contest_rust/abc313/src/bin/a.rs
+++ contest_rust/abc313/src/bin/a.rs
@@ -7,5 +7,10 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: [i64; n],
+    };
+    let ans = 0.max(p[1..].iter().max().unwrap_or(&0) - p.first().unwrap() + 1);
+    println!("{ans}");
 }
