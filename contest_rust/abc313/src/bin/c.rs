e9bb29a7f48328c0e1624c4de8ec01bfcb0eae52
nobushi95 <nobushi1121@gmail.com>
2024-05-11 21:32:54 +0900

    add abc313/C

--- contest_rust/abc313/src/bin/c.rs
+++ contest_rust/abc313/src/bin/c.rs
@@ -7,5 +7,14 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [i64; n],
+    };
+    a.sort();
+    let sum = a.iter().sum::<i64>();
+    let mut b = vec![sum / n as i64; n];
+    b[n - sum as usize % n..].iter_mut().for_each(|x| *x += 1);
+    let ans = a.iter().zip(b).map(|(&a, b)| (a - b).abs()).sum::<i64>() / 2;
+    println!("{ans}");
 }
