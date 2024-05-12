c7ae9d802c682901f1c7a96956c8c603603e6c9d
nobushi95 <nobushi1121@gmail.com>
2024-05-12 15:33:47 +0900

    add abc314/C

--- contest_rust/abc314/src/bin/c.rs
+++ contest_rust/abc314/src/bin/c.rs
@@ -2,10 +2,27 @@ use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::{
     cmp::*,
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
 };
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: u64,
+        s: Chars,
+        c: [u64; n],
+    };
+    let mut tbl = HashMap::<u64, VecDeque<char>>::new();
+    for i in 0..n {
+        tbl.entry(c[i]).or_insert(VecDeque::new()).push_back(s[i]);
+    }
+    for (_, ss) in &mut tbl {
+        let back = ss.pop_back().unwrap();
+        ss.push_front(back);
+    }
+    let ans = c
+        .iter()
+        .map(|x| tbl.get_mut(x).unwrap().pop_front().unwrap())
+        .join("");
+    println!("{ans}");
 }
