6eef641fed19edab33081e6c8082ed35a73cdc9a
nobushi95 <nobushi1121@gmail.com>
2024-05-13 00:11:57 +0900

    add abc314/B

--- contest_rust/abc314/src/bin/b.rs
+++ contest_rust/abc314/src/bin/b.rs
@@ -7,5 +7,36 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! { n: usize };
+    let mut a = vec![vec![]; n];
+    for i in 0..n {
+        input! {
+            c: usize,
+            ai: [u64; c],
+        };
+        a[i] = ai;
+    }
+    input! { x: u64 };
+    let b_candidate = a
+        .iter()
+        .enumerate()
+        .filter(|(_, ai)| ai.contains(&x))
+        .collect_vec();
+
+    let min = b_candidate
+        .iter()
+        .min_by(|l, r| l.1.len().cmp(&r.1.len()))
+        .unwrap_or(&(0, &vec![]))
+        .1
+        .len();
+
+    let b = b_candidate
+        .iter()
+        .filter(|(_, bi)| bi.len() == min)
+        .map(|(i, _)| i + 1)
+        .sorted()
+        .collect_vec();
+    println!("{}", b.len());
+    let ans = b.iter().join(" ");
+    println!("{ans}");
 }
