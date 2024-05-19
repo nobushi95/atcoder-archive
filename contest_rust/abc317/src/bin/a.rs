522252cbb7d518942e43709f79533909ea4db1e3
nobushi95 <nobushi1121@gmail.com>
2024-05-20 01:04:05 +0900

    add abc317/A

--- contest_rust/abc317/src/bin/a.rs
+++ contest_rust/abc317/src/bin/a.rs
@@ -7,5 +7,10 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, h: u64, x: u64,
+        p: [u64; n],
+    };
+    let ans = p.iter().map(|pi| pi + h).position(|pi| pi >= x).unwrap() + 1;
+    println!("{ans}");
 }
