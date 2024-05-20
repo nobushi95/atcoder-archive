2d77983d3078ee3a625d27046fd1091140285164
nobushi95 <nobushi1121@gmail.com>
2024-05-21 00:07:32 +0900

    add abc318/C

--- contest_rust/abc318/src/bin/c.rs
+++ contest_rust/abc318/src/bin/c.rs
@@ -7,5 +7,23 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, d: u64, p: u64,
+        mut f: [u64; n],
+    };
+    f.sort();
+    let s = f
+        .iter()
+        .scan(0, |state, &x| {
+            *state = *state + x;
+            Some(*state)
+        })
+        .collect_vec();
+
+    let k = (n as u64 + d - 1) / d;
+    let mut ans = p * k;
+    for i in 0..k {
+        ans = ans.min(s[n - 1 - (i * d) as usize] + (p * i));
+    }
+    println!("{ans}");
 }
