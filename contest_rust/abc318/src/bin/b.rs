17c64ce11975467b4a7907452130ce70f9b6a620
nobushi95 <nobushi1121@gmail.com>
2024-05-22 00:11:59 +0900

    add abc318/B

--- contest_rust/abc318/src/bin/b.rs
+++ contest_rust/abc318/src/bin/b.rs
@@ -7,5 +7,20 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        abcd: [(u64, u64, u64, u64); n],
+    };
+    let mut s = 0;
+    for i in 0..100_u64 {
+        for j in 0..100_u64 {
+            if abcd
+                .iter()
+                .any(|(a, b, c, d)| *a <= i && i < *b && *c <= j && j < *d)
+            {
+                s += 1;
+            }
+        }
+    }
+    println!("{s}");
 }
