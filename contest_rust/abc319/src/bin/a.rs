7d2db465109bb79abcbae81f583539b1c2234958
nobushi95 <nobushi1121@gmail.com>
2024-05-25 19:44:09 +0900

    create abc319/A

--- contest_rust/abc319/src/bin/a.rs
+++ contest_rust/abc319/src/bin/a.rs
@@ -7,5 +7,20 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! { s: String };
+    let tbl_vec = vec![
+        ("tourist", 3858),
+        ("ksun48", 3679),
+        ("Benq", 3658),
+        ("Um_nik", 3648),
+        ("apiad", 3638),
+        ("Stonefeang", 3630),
+        ("ecnerwala", 3613),
+        ("mnbvmar", 3555),
+        ("newbiedmy", 3516),
+        ("semiexp", 3481),
+    ];
+    let tbl = tbl_vec.iter().cloned().collect::<HashMap<_, _>>();
+    let ans = tbl.get(s.as_str()).unwrap();
+    println!("{ans}");
 }
