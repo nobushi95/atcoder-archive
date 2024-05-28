e051a58f406a3bc50b838e8295b61d8a28553139
nobushi95 <nobushi1121@gmail.com>
2024-05-29 00:28:55 +0900

    add abc321/B

--- contest_rust/abc321/src/bin/b.rs
+++ contest_rust/abc321/src/bin/b.rs
@@ -1,11 +1,27 @@
 use itertools::Itertools;
 use proconio::{input, marker::*};
 use std::{
+    clone,
     cmp::*,
     collections::{HashMap, HashSet},
 };
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, x: u64,
+        a: [u64; n - 1],
+    }
+    let mut ans = -1;
+    for i in 0..=100 {
+        let mut cur = a.clone();
+        cur.push(i);
+        cur.sort();
+        let sum = cur[1..cur.len() - 1].iter().sum::<u64>();
+        if sum >= x {
+            ans = i as i64;
+            break;
+        }
+    }
+    println!("{ans}");
 }
