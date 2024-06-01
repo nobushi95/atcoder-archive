9f12d39c68060e4cf2ebd433aab71716205e1ab5
nobushi95 <nobushi1121@gmail.com>
2024-06-01 23:52:04 +0900

    add abc322/B

--- contest_rust/abc322/src/bin/b.rs
+++ contest_rust/abc322/src/bin/b.rs
@@ -7,5 +7,21 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        s: String,
+        t: String,
+    };
+    let t_pre = t[0..n].chars().join("");
+    let t_suff = t[m - n..].chars().join("");
+    let ans = if t_pre == s && t_suff == s {
+        0
+    } else if t_pre == s && t_suff != s {
+        1
+    } else if t_pre != s && t_suff == s {
+        2
+    } else {
+        3
+    };
+    println!("{ans}");
 }
