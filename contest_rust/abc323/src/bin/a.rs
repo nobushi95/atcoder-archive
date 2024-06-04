edeac4a427f62ad669aaaa0e2ca10b77819f2ae3
nobushi95 <nobushi1121@gmail.com>
2024-06-05 00:20:48 +0900

    add abc323/A

--- contest_rust/abc323/src/bin/a.rs
+++ contest_rust/abc323/src/bin/a.rs
@@ -7,5 +7,18 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    };
+    let ans = if s
+        .iter()
+        .enumerate()
+        .all(|(i, &x)| if (i + 1) % 2 == 0 { x == '0' } else { true })
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{ans}");
 }
