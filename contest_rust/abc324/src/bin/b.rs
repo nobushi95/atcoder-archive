d435881e6415074f7a81dbc00248daf4aa655fb4
nobushi95 <nobushi1121@gmail.com>
2024-06-06 00:07:45 +0900

    add abc324/B

--- contest_rust/abc324/src/bin/b.rs
+++ contest_rust/abc324/src/bin/b.rs
@@ -7,5 +7,18 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        mut n: u64,
+    };
+
+    while n % 2 == 0 {
+        n /= 2;
+    }
+
+    while n % 3 == 0 {
+        n /= 3;
+    }
+
+    let ans = if n == 1 { "Yes" } else { "No" };
+    println!("{ans}");
 }
