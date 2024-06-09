3bbc818bece52767da2faab54ecf8c6c6156fca5
nobushi95 <nobushi1121@gmail.com>
2024-06-09 23:45:28 +0900

    add abc325/B

--- contest_rust/abc325/src/bin/b.rs
+++ contest_rust/abc325/src/bin/b.rs
@@ -7,5 +7,21 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        wx: [(u64, u64); n],
+    };
+    let mut ans = 0;
+    for i in 0..24 {
+        let cur = wx
+            .iter()
+            .filter(|(_, x)| {
+                let local_time = (*x + i) % 24;
+                local_time >= 9 && local_time < 18
+            })
+            .map(|(w, _)| *w)
+            .sum::<u64>();
+        ans = ans.max(cur);
+    }
+    println!("{ans}");
 }
