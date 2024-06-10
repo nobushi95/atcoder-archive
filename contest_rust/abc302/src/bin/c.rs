7c606cdbe260ead675842f926334bfc29c985c0b
nobushi95 <nobushi1121@gmail.com>
2024-06-11 00:28:54 +0900

    add abc302/C

--- contest_rust/abc302/src/bin/c.rs
+++ contest_rust/abc302/src/bin/c.rs
@@ -3,5 +3,24 @@ use proconio::{input, marker::*};
 use std::cmp::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: usize,
+        s: [String; n],
+    };
+    let perm = s.iter().permutations(n);
+
+    for p in perm {
+        if p.windows(2).all(|x| {
+            x[0].chars()
+                .zip(x[1].chars())
+                .filter(|(l, r)| l != r)
+                .count()
+                == 1
+        }) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
