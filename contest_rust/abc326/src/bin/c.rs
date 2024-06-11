28cd52342905d80a2e26eb16b6a47d92ffcdd753
nobushi95 <nobushi1121@gmail.com>
2024-06-12 00:59:30 +0900

    add abc326/C

--- contest_rust/abc326/src/bin/c.rs
+++ contest_rust/abc326/src/bin/c.rs
@@ -7,5 +7,15 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: u64,
+        mut a: [u64; n],
+    };
+    a.sort();
+    let mut ans = 0;
+    for i in 0..n {
+        let cur = a.lower_bound(&(a[i] + m)) - a.lower_bound(&a[i]);
+        ans = ans.max(cur);
+    }
+    println!("{ans}");
 }
