93038e5f2e12337bccc62a997061da712042009d
nobushi95 <nobushi1121@gmail.com>
2024-06-15 20:53:25 +0900

    add abc300/C

--- contest_cpp/abc300/C.cpp
+++ contest_cpp/abc300/C.cpp
@@ -14,7 +14,41 @@ const int inf = INT_MAX / 2;
 template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
 template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
 
+vector<pair<ll, ll>> d = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
 int main() {
-    
+    ll H, W;
+    cin >> H >> W;
+    auto C = vector<string>(H);
+    REP(i, 0, H) {
+        cin >> C[i];
+    }
+    auto n = min(H, W);
+    map<ll, ll> ans;
+
+    REP(y, 0, H) REP(x, 0, W) {
+        if (C[y][x] != '#') continue;
+
+        REPEQ(k, 1, n + 1) {
+            auto is_cross = true;
+            REP(l, 0, 4) {
+                auto nx = d[l].first * k + x;
+                auto ny = d[l].second * k + y;
+                if (nx < 0 || nx >= W || ny < 0 || ny >= H || C[ny][nx] != '#') {
+                    is_cross = false;
+                    break;
+                }
+            }
+            if (!is_cross) {
+                ans[k - 1]++;
+                break;
+            }
+        }
+    }
+
+    REPEQ(i, 1, n) {
+        if (i == n) cout << ans[i] << endl;
+        else cout << ans[i] << " ";
+    }
     return 0;
 }
