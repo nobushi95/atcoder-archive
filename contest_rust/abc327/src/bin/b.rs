66855fa3bdccc46bae878648fadaa8d162dbf747
nobushi95 <nobushi1121@gmail.com>
2024-06-17 00:14:37 +0900

    add abc327/B

--- contest_rust/abc327/src/bin/b.rs
+++ contest_rust/abc327/src/bin/b.rs
@@ -7,5 +7,17 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! { b: u64 };
+
+    let mut ans = -1;
+    let mut a = 1_u64;
+    while a.pow(a as u32) <= b {
+        if a.pow(a as u32) == b {
+            ans = a as i64;
+            break;
+        }
+        a += 1;
+    }
+
+    println!("{ans}");
 }
