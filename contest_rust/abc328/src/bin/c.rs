68b63e8415a49e0f5442e5a438f9576910b6fb9b
nobushi95 <nobushi1121@gmail.com>
2024-06-19 00:48:31 +0900

    add abc328/C

--- contest_rust/abc328/src/bin/c.rs
+++ contest_rust/abc328/src/bin/c.rs
@@ -7,5 +7,25 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, q: usize,
+        s: Chars,
+        lr: [(usize, usize); q],
+    };
+    let acc = s
+        .windows(2)
+        .map(|x| x[0] == x[1])
+        .scan(0, |state, x| {
+            if x {
+                *state += 1;
+            }
+            Some(*state)
+        })
+        .collect_vec();
+    for (l, r) in lr {
+        let l = if l as i32 - 2 < 0 { 0 } else { acc[l - 2] };
+        let r = if r as i32 - 2 < 0 { 0 } else { acc[r - 2] };
+        let ans = r - l;
+        println!("{ans}");
+    }
 }
