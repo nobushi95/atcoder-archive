fb6bdc6033033e4d908c8bf1a9720c474ec0adb2
nobushi95 <nobushi1121@gmail.com>
2024-06-23 02:46:39 +0900

    add abc329/C

--- contest_rust/abc329/src/bin/c.rs
+++ contest_rust/abc329/src/bin/c.rs
@@ -7,5 +7,22 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+         n: usize,
+         s: Chars,
+    };
+    let mut mx = vec![0; 26];
+    let mut l = 0;
+    while l < n {
+        let mut r = l + 1;
+        while r < n && s[l] == s[r] {
+            r += 1;
+        }
+        let c = (s[l] as u8 - b'a') as usize;
+        mx[c] = mx[c].max(r - l);
+        l = r;
+    }
+
+    let ans = mx.iter().sum::<usize>();
+    println!("{ans}");
 }
