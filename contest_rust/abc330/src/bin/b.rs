52748acd5dd8260ecd3bb9860fee69a83ea0ab65
nobushi95 <nobushi1121@gmail.com>
2024-06-26 00:03:37 +0900

    add abc330/B

--- contest_rust/abc330/src/bin/b.rs
+++ contest_rust/abc330/src/bin/b.rs
@@ -7,5 +7,10 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, l: u64, r: u64,
+        a: [u64; n],
+    };
+    let ans = a.iter().map(|x| x.clamp(&l, &r)).join(" ");
+    println!("{ans}");
 }
