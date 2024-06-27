734076ab0cda5cf2481f941e05c61a41abfcfd0a
nobushi95 <nobushi1121@gmail.com>
2024-06-28 00:34:01 +0900

    add abc331/B

--- contest_rust/abc331/src/bin/b.rs
+++ contest_rust/abc331/src/bin/b.rs
@@ -7,5 +7,19 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: u64, s: u64, m: u64, l: u64
+    };
+
+    let mut ans = u64::MAX;
+    for s_num in 0..=100 {
+        for m_num in 0..=100 {
+            for l_num in 0..=100 {
+                if s_num * 6 + m_num * 8 + l_num * 12 >= n {
+                    ans = ans.min(s * s_num + m * m_num + l * l_num);
+                }
+            }
+        }
+    }
+    println!("{ans}");
 }
