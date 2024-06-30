f6528124a4da7c3cb3ebcbc3186b24a556245381
nobushi95 <nobushi1121@gmail.com>
2024-07-01 00:50:45 +0900

    add abc332/C

--- contest_rust/abc332/src/bin/c.rs
+++ contest_rust/abc332/src/bin/c.rs
@@ -7,5 +7,29 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize, m: i64,
+        mut s: Chars,
+    };
+    s.push('0');
+    let mut ans = 0;
+    let mut x = 0;
+    let mut y = 0;
+    for si in s {
+        match si {
+            '0' => {
+                ans = ans.max(y.max(x + y - m));
+                x = 0;
+                y = 0;
+            }
+            '1' => {
+                x += 1;
+            }
+            '2' => {
+                y += 1;
+            }
+            _ => unreachable!(),
+        };
+    }
+    println!("{ans}");
 }
