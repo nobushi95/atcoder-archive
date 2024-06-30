22f1ba4645b98330c6b145708f5a4208eaec9a9a
nobushi95 <nobushi1121@gmail.com>
2024-06-30 19:39:53 +0900

    add abc331/C

--- contest_rust/abc331/src/bin/c.rs
+++ contest_rust/abc331/src/bin/c.rs
@@ -7,5 +7,22 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+    };
+    let mut tbl = HashMap::<i64, Vec<_>>::new();
+    for i in 0..n {
+        tbl.entry(a[i]).or_insert(vec![]).push(i);
+    }
+    let mut sum = a.iter().sum::<i64>();
+    let mut ans = vec![0; n];
+    for v in tbl.keys().sorted() {
+        sum -= v * (tbl[v].len() as i64);
+        for &i in tbl[v].iter() {
+            ans[i] = sum;
+        }
+    }
+    let ans = ans.iter().join(" ");
+    println!("{ans}");
 }
