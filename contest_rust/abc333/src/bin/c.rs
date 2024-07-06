5afd0bb3b4778136a9467eb8dc83661a1add4831
nobushi95 <nobushi1121@gmail.com>
2024-07-06 14:16:59 +0900

    add abc333/C

--- contest_rust/abc333/src/bin/c.rs
+++ contest_rust/abc333/src/bin/c.rs
@@ -7,5 +7,21 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! { mut n: u64 };
+
+    for d in 1..=12 {
+        for a in (0..=(d - 1)).rev() {
+            for b in (0..=(d - a - 1)).rev() {
+                let c = d - a - b;
+                n -= 1;
+                if n == 0 {
+                    let ans = (0..a).map(|_| '1').join("")
+                        + &(0..b).map(|_| '2').join("")
+                        + &(0..c).map(|_| '3').join("");
+                    println!("{ans}");
+                    return;
+                }
+            }
+        }
+    }
 }
