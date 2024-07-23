c69c4524b0d081cf136842d8ac8795d5438ffdc0
nobushi95 <nobushi1121@gmail.com>
2024-07-24 00:04:10 +0900

    add abc337/C

--- contest_rust/abc337/src/bin/c.rs
+++ contest_rust/abc337/src/bin/c.rs
@@ -7,5 +7,23 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+    };
+    let tbl = a
+        .iter()
+        .enumerate()
+        .map(|(i, &x)| (x, (i + 1) as i64))
+        .collect::<HashMap<_, _>>();
+
+    let mut cur = tbl.get(&-1).unwrap().clone();
+    let mut ans = vec![cur];
+    for _ in 0..n - 1 {
+        cur = tbl.get(&cur).unwrap().clone();
+        ans.push(cur);
+    }
+
+    let ans = ans.iter().join(" ");
+    println!("{ans}");
 }
