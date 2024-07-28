334e7635fe24882cebfeb5c2bc35dc57cda62076
nobushi95 <nobushi1121@gmail.com>
2024-07-29 00:10:39 +0900

    add abc338/A

--- contest_rust/abc338/src/bin/a.rs
+++ contest_rust/abc338/src/bin/a.rs
@@ -7,5 +7,11 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! { s: Chars };
+    let ans = if s.first().unwrap().is_uppercase() && s[1..].iter().all(|&x| x.is_lowercase()) {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{ans}");
 }
