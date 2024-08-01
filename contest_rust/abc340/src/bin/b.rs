8c87e6e6ac81743eac8d80658c2aecb501a27770
nobushi95 <nobushi1121@gmail.com>
2024-08-02 00:21:29 +0900

    add abc340/B

--- contest_rust/abc340/src/bin/b.rs
+++ contest_rust/abc340/src/bin/b.rs
@@ -7,5 +7,17 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! { q: usize };
+    let mut v = vec![];
+    for _ in 0..q {
+        input! { c: i64 };
+        if c == 1 {
+            input! { x: i64 };
+            v.push(x);
+        } else if c == 2 {
+            input! { k: i64 };
+            let ans = v[v.len() - k as usize];
+            println!("{ans}");
+        }
+    }
 }
