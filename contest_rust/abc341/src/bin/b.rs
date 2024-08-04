1653c25be4e6102ec48e97250d8d621eb2f5720c
nobushi95 <nobushi1121@gmail.com>
2024-08-04 23:33:23 +0900

    add abc341/B

--- contest_rust/abc341/src/bin/b.rs
+++ contest_rust/abc341/src/bin/b.rs
@@ -7,5 +7,17 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [i64; n],
+        st: [(i64, i64); n - 1],
+    };
+
+    for i in 0..n - 1 {
+        let (s, t) = st[i];
+        a[i + 1] += a[i] / s * t;
+    }
+
+    let ans = a.last().unwrap();
+    println!("{ans}");
 }
