d0e3ab4a6f62225357b4162bfba2b2888edeae48
nobushi95 <nobushi1121@gmail.com>
2024-08-06 00:03:00 +0900

    add abc342/B

--- contest_rust/abc342/src/bin/b.rs
+++ contest_rust/abc342/src/bin/b.rs
@@ -7,5 +7,25 @@ use std::{
 use superslice::*;
 
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: [usize; n],
+        q: usize,
+        ab: [(usize, usize); q],
+    };
+    let tbl = p
+        .iter()
+        .enumerate()
+        .map(|(i, x)| (*x, i))
+        .collect::<HashMap<_, _>>();
+
+    let a = ab.iter().map(|&(a, _)| a).collect_vec();
+    let b = ab.iter().map(|&(_, b)| b).collect_vec();
+
+    for i in 0..q {
+        let a_i = tbl.get(&a[i]).unwrap();
+        let b_i = tbl.get(&b[i]).unwrap();
+        let ans = if a_i < b_i { a[i] } else { b[i] };
+        println!("{ans}");
+    }
 }
